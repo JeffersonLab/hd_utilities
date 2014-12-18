d2790c25f181c532e364c9145623fc8a7829f037
kmoriya <kmoriya@jlab.org>
2014-12-18 12:52:19 +0000

    get magnetic field setting from Sean's database
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@16918 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- batch/generatejobs_plugins_rawdata.sh
+++ batch/generatejobs_plugins_rawdata.sh
@@ -40,7 +40,7 @@ set MINRUN     = $1
 set MAXRUN     = $2
 # File #s to iterate over for this run.
 set MINFILE =   0
-set MAXFILE = 999
+set MAXFILE =  19
 
 set INPUT2 = $3
 set INPUT3 = $4
@@ -54,18 +54,18 @@ if ( $INPUT3 != "" ) then
 endif
 
 if ($1 == "" ) then
-  echo "gen_requests.sh"
+  echo "generatejobs_plugins_rawdata.sh"
   echo "Usage:"
   echo "generatejobs_plugins_rawdata.sh [minrun] [maxrun]"
   exit
 endif
 
-source /home/gluex/setup_jlab_commissioning.csh
+source /home/gxproj1/setup_jlab.csh
 
 # Get svn revision number to put in output file name
 cd $HDDS_HOME
 set svn_hdds = `svn info | grep 'Revision' | gawk '{print $2}'`
-echo "hdds revision = ${svn_hdds}"
+echo "hdds revision      = ${svn_hdds}"
 cd -
 
 cd $HALLD_HOME
@@ -73,9 +73,9 @@ set svn_sim_recon = `svn info | grep 'Revision' | gawk '{print $2}'`
 echo "sim-recon revision = ${svn_sim_recon}"
 cd -
 
-cd $HALLD_HOME/../../online/monitoring/src/plugins
+cd /home/gxproj1/builds/online/packages/monitoring/src/plugins
 set svn_plugins = `svn info | grep 'Revision' | gawk '{print $2}'`
-echo "plugins revision = ${svn_plugins}"
+echo "plugins revision   = ${svn_plugins}"
 cd -
 
 # set date
@@ -115,6 +115,7 @@ echo "</gversions>" >> $XMLFILE
 ########################################################
 
 set RUN = ${MINRUN}
+
 while (${RUN} <= ${MAXRUN})
 	set FORMATTED_RUN = `printf %06d $RUN`
 	echo "----------    Run = ${FORMATTED_RUN}   ---------------"
@@ -135,37 +136,219 @@ while (${RUN} <= ${MAXRUN})
           set INPUT_PREFIX    = "hd_raw_${FORMATTED_RUN}_"
 	endif
 	set INPUT_SUFFIX      = ".evio"
-	set THIS_DIR          = $PWD
+	set THIS_DIR          = /home/gxproj1/halld/monitoring/batch/
 	set SCRIPT_OUTPUT_DIR = "${THIS_DIR}/runs/${FORMATTED_RUN}/"
-	set OUTDIR            = "/volatile/halld/RunPeriod-2014-10/offline_monitoring/${FORMATTED_RUN}/${DATE}"
-	set PLUGINS           = "DAQ,TTab,TAGH_online,TAGM_online,BCAL_online,CDC_online,CDC_expert,FCAL_online,FDC_online,ST_online,TOF_online,monitoring_hists,evio_writer,2trackskim"
+	set OUTDIR            = "/volatile/halld/RunPeriod-2014-10/offline_monitoring/${DATE}"
+	set PLUGINS           = "DAQ,TTab,TAGH_online,TAGM_online,BCAL_online,CDC_online,CDC_expert,FCAL_online,FDC_online,ST_online,TOF_online,monitoring_hists,evio_writer,2trackskim,TOF_TDC_shift"
 #,PS_online,PSC_online"
-	set SCRIPTFILE        = "/home/gluex/halld/monitoring/batch/script.sh"
-	set SOURCEFILE        = "/home/gluex/setup_jlab_commissioning.csh"
+	set SCRIPTFILE        = "/home/gxproj1/halld/monitoring/batch/script.sh"
+	set SOURCEFILE        = "/home/gxproj1/setup_jlab.csh"
 	set TIME              = 1440 # in minutes
-	set DISKSPACE         =  5 # in GB
+	set DISKSPACE         =  8 # in GB
 	set MEMORY            =  3 # in GB
-	set EMAIL             = "gluex@jlab.org"
+	set EMAIL             = "gxproj1@jlab.org"
 #	set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
 	set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
 	set JANAFILE_SUFFIX   = "_1000A"
         set ADDITIONAL_OPTION = ""
-	if($RUN == 9101) then
-	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
-	  set JANAFILE_SUFFIX   = "_1200A"
-	else if($RUN > 1129 && $RUN < 1449) then
-	  set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+
+	# Get magnetic field setting from Sean's database
+        set BFIELD_MEAS = `mysql -sN -h hallddb -u datmon -e "select solenoid_current from run_info where run_num=$RUN" data_monitoring`
+	if ( ${BFIELD_MEAS} != "NULL") then
+          set BFIELD_MEAS_INT = `python -c "from math import ceil; print int(ceil($BFIELD_MEAS))"`
+        else
+          set BFIELD_MEAS_INT = "-999"
+        endif
+
+	if( ${BFIELD_MEAS_INT} < 25 ) then
+	  set BFIELD_OPTION     = "-PBFIELD_TYPE=NoField"
           set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
-          set JANAFILE_SUFFIX   = "_0000A"
-	else if($RUN > 1448) then
-	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+	  set JANAFILE_SUFFIX   = "_0000A"
+	else if ( ${BFIELD_MEAS_INT} >= 25 && ${BFIELD_MEAS_INT} < 75) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_50A_poisson_20140819"
           set ADDITIONAL_OPTION = ""
-          set JANAFILE_SUFFIX   = "_1200A"
-	#else
-	#  echo "run must be 9101 or 9102"
-	#  exit
-	endif
-	#---------------------------------------------------------------------------------------
+	  set JANAFILE_SUFFIX   = "_0050A"
+	else if ( ${BFIELD_MEAS_INT} >= 75 && ${BFIELD_MEAS_INT} < 150) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_100A_poisson_20140819"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_0100A"
+	else if ( ${BFIELD_MEAS_INT} >= 150 && ${BFIELD_MEAS_INT} < 250) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_200A_poisson_20140819"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_0200A"
+	else if ( ${BFIELD_MEAS_INT} >= 250 && ${BFIELD_MEAS_INT} < 450) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_0300A"
+	else if ( ${BFIELD_MEAS_INT} >= 450 && ${BFIELD_MEAS_INT} < 675) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_600A_poisson_20141118"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_0600A"
+	else if ( ${BFIELD_MEAS_INT} >= 675 && ${BFIELD_MEAS_INT} < 875) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_750A_poisson_20140314"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_0750A"
+	else if ( ${BFIELD_MEAS_INT} >= 875 && ${BFIELD_MEAS_INT} < 1100) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_1000A"
+	else if ( ${BFIELD_MEAS_INT} >= 1100 && ${BFIELD_MEAS_INT} < 1275) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_1200A"
+	else if ( ${BFIELD_MEAS_INT} >= 1275 && ${BFIELD_MEAS_INT} < 1425) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1350_poisson_20130925"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_1350A"
+	else if ( ${BFIELD_MEAS_INT} >= 1425) then
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1500A_poisson_20140520"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_1350A"
+	else if ( ${BFIELD_MEAS_INT} == -999 ) then
+	  echo "NO MAGNETIC FIELD INFO IN DATABASE... SETTING TO DEFAULT 1200 A"
+	  set BFIELD_OPTION     = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+          set ADDITIONAL_OPTION = ""
+	  set JANAFILE_SUFFIX   = "_1200A"
+	else
+	  echo "VALUE UNKNOWN"
+	  echo "RUN = $RUN, BFIELD_MEAS = ${BFIELD_MEAS}"
+	  exit
+        endif
+
+#	if($RUN == 9101) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+#	  set JANAFILE_SUFFIX   = "_1200A"
+#	else if($RUN >= 940) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
+#          set JANAFILE_SUFFIX   = "_1000A"
+#	else if($RUN >= 1036 && $RUN <= 1053) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#	else if($RUN >= 1065 && $RUN <= 1121) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#	else if($RUN >= 1122 && $RUN <= 1211) then
+#	  set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#	else if($RUN >= 1212 && $RUN <= 1254) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#	else if($RUN >= 1255 && $RUN <= 1308) then
+#	  set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#	else if($RUN >= 1309 && $RUN <= 1318) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#	else if($RUN >= 1319 && $RUN <= 1448) then
+#	  set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#	else if($RUN >= 1449 && $RUN <= 1643) then
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+#          set JANAFILE_SUFFIX   = "_1200A"
+#        else if($RUN >= 1644 && $RUN <= 1645) then
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#        else if($RUN >= 1646 && $RUN <= 1650) then
+#          # actually between 150 - 260
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#        else if($RUN >= 1651 && $RUN <= 1655) then
+#          set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#        else if($RUN == 1656) then
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#        else if($RUN >= 1657 && $RUN <= 1675) then
+#          set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#        else if($RUN == 1676) then
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_100A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0100A"
+#        else if($RUN >= 1677 && $RUN <= 1696) then
+#          # actually 800 A
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_750A_poisson_20140314"
+#          set JANAFILE_SUFFIX   = "_0750A"
+#        else if($RUN >= 1697 && $RUN <= 1698) then
+#          # actually 950 A
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
+#          set JANAFILE_SUFFIX   = "_1000A"
+#        else if($RUN >= 1699 && $RUN <= 1810) then
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+#          set JANAFILE_SUFFIX   = "_1200A"
+#        else if($RUN >= 1811 && $RUN <= 1823) then
+#          set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#        else if($RUN >= 1824 && $RUN <= 1845) then
+#          # ramping from 0 A to 950 A
+#          set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#        else if($RUN >= 1846 && $RUN <= 1876) then
+#          # actually 150 A
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_200A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0200A"
+#        else if($RUN >= 1877 && $RUN <= 1904) then
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#        else if($RUN == 1905) then
+#          # actually 800 A
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
+#          set JANAFILE_SUFFIX   = "_1000A"
+#	else if($RUN >= 1906 && $RUN <= 2016) then
+#	  # ramping from 0 A to 950 A
+#	  set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#	else if($RUN >= 2017 && $RUN <= 2022) then
+#	  # actually ramping from 230 A - 800 A
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_750A_poisson_20140314"
+#          set JANAFILE_SUFFIX   = "_0750A"
+#	else if($RUN >= 2023 && $RUN <= 2029) then
+#	  # actually ramping from 1000 A - 1200 A
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
+#          set JANAFILE_SUFFIX   = "_1000A"
+#	else if($RUN >= 2030 && $RUN <= 2223) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+#          set JANAFILE_SUFFIX   = "_1200A"
+#	else if($RUN >= 2224 && $RUN <= 2229) then
+#	  # actually ramping down from 1000 A - 800 A
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
+#          set JANAFILE_SUFFIX   = "_1000A"
+#        else if($RUN == 2230) then
+#	  # during ramp down
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_300A_poisson_20140819"
+#          set JANAFILE_SUFFIX   = "_0300A"
+#	else if($RUN >= 2231 && $RUN <= 2246) then
+#	  set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#	else if($RUN >= 2247 && $RUN <= 2250) then
+#	  # actually ramping from 650 A - 845 A
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_750A_poisson_20140314"
+#          set JANAFILE_SUFFIX   = "_0750A"
+#	else if($RUN >= 2251 && $RUN <= 2261) then
+#	  # actually ramping down from 900 A - 1078 A
+#          set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1000A_poisson_20141104"
+#          set JANAFILE_SUFFIX   = "_1000A"
+#	else if($RUN >= 2262) then
+#	  set BFIELD_OPTION   = "-PBFIELD_MAP=Magnets/Solenoid/solenoid_1200A_poisson_20140520"
+#          set JANAFILE_SUFFIX   = "_1200A"
+#	else
+#	  echo "-----------------------------------------------"
+#	  echo "run $RUN does not have magnetic field set"
+#	  echo "using 0 A setting..."
+#	  echo "-----------------------------------------------"
+#	  set BFIELD_OPTION   = "-PBFIELD_TYPE=NoField"
+#          set ADDITIONAL_OPTION = "-PDEFTAG:DTrackCandidate=StraightLine"
+#          set JANAFILE_SUFFIX   = "_0000A"
+#	endif
+#	#---------------------------------------------------------------------------------------
 
 	# Create jana config file
 	set JANAFILE = "/work/halld/data_monitoring/run_conditions/jana_rawdata_comm_${YEAR}_${MONTH}_${DAY}.conf"
@@ -174,7 +357,7 @@ while (${RUN} <= ${MAXRUN})
 	rm -f $JANAFILE
 	endif
 
-	echo "-PPLUGINS=DAQ,TTab,TAGH_online,TAGM_online,BCAL_online,CDC_online,CDC_expert,FCAL_online,FDC_online,ST_online,TOF_online,monitoring_hists,evio_writer,2trackskim" > $JANAFILE
+	echo "-PPLUGINS=DAQ,TTab,TAGH_online,TAGM_online,BCAL_online,CDC_online,CDC_expert,FCAL_online,FDC_online,ST_online,TOF_online,monitoring_hists,evio_writer,2trackskim,TOF_TDC_shift" > $JANAFILE
 	echo ${BFIELD_OPTION} ${ADDITIONAL_OPTION} >> $JANAFILE
 	echo "-PNTHREADS=1" >> $JANAFILE
 	echo "-PTHREAD_TIMEOUT=300" >> $JANAFILE
@@ -191,16 +374,23 @@ while (${RUN} <= ${MAXRUN})
 	echo "Bfield option     : ${BFIELD_OPTION}"
 	echo "additional option : ${ADDITIONAL_OPTION}"     
 
+	mkdir -p ${THIS_DIR}/runscripts
 	set RUN_SCRIPT = "${THIS_DIR}/runscripts/run_rawdata_${FORMATTED_RUN}.sh"
 	if( -e $RUN_SCRIPT ) then
 	  rm -f $RUN_SCRIPT
 	endif
 
 	echo "" > $RUN_SCRIPT
+	ls -lh $RUN_SCRIPT
 
 	mkdir -p ${SCRIPT_OUTPUT_DIR}
 	mkdir -p ${OUTDIR}
-	mkdir -p ${OUTDIR}/log
+	mkdir -p ${OUTDIR}/ROOT/${FORMATTED_RUN}
+	mkdir -p ${OUTDIR}/skims/${FORMATTED_RUN}
+	mkdir -p ${OUTDIR}/misc/${FORMATTED_RUN}
+	mkdir -p ${OUTDIR}/log/${FORMATTED_RUN}
+	# This is so other gluex group users can read/write
+	chmod g+rw ${OUTDIR} ${OUTDIR}/ROOT ${OUTDIR}/skims ${OUTDIR}/misc ${OUTDIR}/log ${OUTDIR}/ROOT/${FORMATTED_RUN} ${OUTDIR}/skims/${FORMATTED_RUN} ${OUTDIR}/misc/${FORMATTED_RUN} ${OUTDIR}/log/${FORMATTED_RUN}
 
 	# Create ls file. This is just all the runs within the
 	# input directory.
@@ -213,8 +403,7 @@ while (${RUN} <= ${MAXRUN})
           continue
         endif
 
-	echo "AAA"
-
+	mkdir -p ${THIS_DIR}/lsfiles/
 	set LSFILE = "${THIS_DIR}/lsfiles/ls-evio_${FORMATTED_RUN}"
 	if ( -e $LSFILE ) then
 	  rm -f $LSFILE
@@ -225,7 +414,7 @@ while (${RUN} <= ${MAXRUN})
 
 	# Create input list file. This just tracks which
 	# files were present.
-	set LISTFILE = ${OUTDIR}/infiles_${FORMATTED_RUN}.txt
+	set LISTFILE = ${OUTDIR}/misc/${FORMATTED_RUN}/infiles_${FORMATTED_RUN}.txt
 	if ( -e $LISTFILE ) then
 	  rm -f $LISTFILE
 	endif
@@ -243,7 +432,7 @@ while (${RUN} <= ${MAXRUN})
 		echo '	<Variable name="formatted_run" value="'${FORMATTED_RUN}'"/>' >> ${OUTPUT_FILE}
 		echo '	<Variable name="output_dir_base" value="file:'${OUTDIR}'"/>' >> ${OUTPUT_FILE}
 		echo '	<Variable name="plugins" value="'${PLUGINS}'"/>' >> ${OUTPUT_FILE} # monitoring_hists,
-		echo '	<Variable name="env" value="setup_jlab_commissioning.csh"/>' >> ${OUTPUT_FILE}
+		echo '	<Variable name="env" value="setup_jlab.csh"/>' >> ${OUTPUT_FILE}
 		echo '	<Variable name="svn" value="'${svn_sim_recon}'"/>' >> ${OUTPUT_FILE}
 		echo '	<Variable name="bfield_option" value="'${BFIELD_OPTION}'"/>' >> ${OUTPUT_FILE}
 		echo '	<Variable name="additional_option" value="'${ADDITIONAL_OPTION}'"/>' >> ${OUTPUT_FILE}
@@ -285,11 +474,12 @@ while (${RUN} <= ${MAXRUN})
 		echo '		<Job>' >> ${OUTPUT_FILE}
 		echo '			<Name name="rawdata_plugins.'${FORMATTED_RUN}'_${input_file_stubs}"/>' >> ${OUTPUT_FILE}
 		echo '			<Input src="'${SOURCETYPE}':'${INPUT_DIR}'/'${INPUT_PREFIX}'${input_file_stubs}'${INPUT_SUFFIX}'" dest="'${INPUT_PREFIX}'${input_file_stubs}'${INPUT_SUFFIX}'"/>' >> ${OUTPUT_FILE}
-		echo '			<Command>script.sh ${env} '${INPUT_PREFIX}'${input_file_stubs}'${INPUT_SUFFIX}' ${plugins} ${bfield_option} ${additional_option} ${formatted_run}</Command>' >> ${OUTPUT_FILE}
-		echo '			<Output src="hd_root.root" dest="${output_dir_base}/hd_root_${formatted_run}_${input_file_stubs}.root"/>' >> ${OUTPUT_FILE}
-		echo '			<Output src="'${INPUT_PREFIX}'${input_file_stubs}.2tracks.evio" dest="${output_dir_base}/'${INPUT_PREFIX}'${input_file_stubs}.2tracks.evio"/>' >> ${OUTPUT_FILE}
-		echo '			<Stdout dest="${output_dir_base}/log/stdout_${formatted_run}_${input_file_stubs}.out"/>' >> ${OUTPUT_FILE}
-		echo '			<Stderr dest="${output_dir_base}/log/stderr_${formatted_run}_${input_file_stubs}.err"/>' >> ${OUTPUT_FILE}
+		echo '			<Command>./script.sh ${env} '${INPUT_PREFIX}'${input_file_stubs}'${INPUT_SUFFIX}' ${plugins} ${bfield_option} ${additional_option} ${formatted_run}</Command>' >> ${OUTPUT_FILE}
+		echo '			<Output src="hd_root.root" dest="${output_dir_base}/ROOT/${formatted_run}/hd_root_${formatted_run}_${input_file_stubs}.root"/>' >> ${OUTPUT_FILE}
+		echo '			<Output src="'${INPUT_PREFIX}'${input_file_stubs}.2tracks.evio" dest="${output_dir_base}/skims/${formatted_run}/'${INPUT_PREFIX}'${input_file_stubs}.2tracks.evio"/>' >> ${OUTPUT_FILE}
+		echo '			<Output src="TOF_TDC_shift_${formatted_run}.txt" dest="${output_dir_base}/misc/${formatted_run}/TOF_TDC_shift_${formatted_run}_${input_file_stubs}.txt"/>' >> ${OUTPUT_FILE}
+		echo '			<Stdout dest="${output_dir_base}/log/${formatted_run}/stdout_${formatted_run}_${input_file_stubs}.out"/>' >> ${OUTPUT_FILE}
+		echo '			<Stderr dest="${output_dir_base}/log/${formatted_run}/stderr_${formatted_run}_${input_file_stubs}.err"/>' >> ${OUTPUT_FILE}
 		echo '		</Job>' >> ${OUTPUT_FILE}
 		echo '	</ForEach>' >> ${OUTPUT_FILE}
 
