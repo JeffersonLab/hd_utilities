f0711beeeaa8d3ff58f9caa6617327a288c8b438
kmoriya <kmoriya@jlab.org>
2015-06-10 17:10:18 +0000

    specify run period and version to create txt file from job and aux tables
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@18713 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- jproj/projects/cross_analysis/create_txt_file_from_dbs.sh
+++ jproj/projects/cross_analysis/create_txt_file_from_dbs.sh
@@ -1,25 +1,27 @@
 #!/bin/tcsh
 
-if ( $1 == "" ) then
-  echo "create_txt_file_from_dbs.sh"
-  echo "Need to specify version..."
+if ( $1 == "" || $2 == "" ) then
+  echo "Usage:"
+  echo "  create_txt_file_from_dbs.sh [run period] [ver #]"
+  echo "example:   create_txt_file_from_dbs.sh 2015_03 03"
   exit
 endif
-set VER = $1
+set RUNPERIOD = $1
+set VER       = $2
 
 # Tables will have names
 # 1. offmon_rp[DATAPERIOD]_v[VER]_type[TYPE]
 # 2. offmon_rp[DATAPERIOD]_v[VER]_type[TYPE]Job
 
 # standard names
-set TABLEJOB = "offline_monitoring_RunPeriod2014_10_ver${VER}_hd_rawdataJob"
-set TABLEAUX = "offline_monitoring_RunPeriod2014_10_ver${VER}_hd_rawdata_aux"
-set OUTFILE  = "jobinfo_ver${VER}.txt"
+set TABLEJOB = "offline_monitoring_RunPeriod${RUNPERIOD}_ver${VER}_hd_rawdataJob"
+set TABLEAUX = "offline_monitoring_RunPeriod${RUNPERIOD}_ver${VER}_hd_rawdata_aux"
+set OUTFILE  = "jobinfo_${RUNPERIOD}_ver${VER}.txt"
 
-if ( $VER == "10" ) then
+if ( $RUNPERIOD == "2014_10" && $VER == "10" ) then
   # for ver10
   set TABLEJOB = "offmon_rp2014m10_v10_type1Job"
-else if ( $VER == "09" ) then
+else if ( $RUNPERIOD == "2014_10" && $VER == "09" ) then
   # for ver09
   set TABLEJOB = "offline_monJob"
 endif
