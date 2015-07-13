6d791ea70d586e2831f3c2630928c40a68361bc6
kmoriya <kmoriya@jlab.org>
2015-07-13 17:35:44 +0000

    file to insert run and file numbers into cross analysis tables
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@19013 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

---
+++ jproj/projects/cross_analysis/write_inserts_cross_analysis.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+$tablename = "cross_analysis_table";
+
+while (<>) {
+    chomp;
+    @t0 = split;
+    $run = $t0[0];
+    $file = $t0[1];
+    print "INSERT INTO $tablename SET run = $run, file = $file;\n";
+}
+exit;
+
