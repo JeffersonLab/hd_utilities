000eebcea5ebf3493c2e4a1b4f2e23ddf80b7142
marki <marki@e333a0bb-b310-0410-bd06-bbc7ae8e47d2>
2015-09-03 13:48:21 +0000

    rename single platform script, use new b1pi_test.sh

--- b1pi_cron_one.sh
+++ b1pi_cron_one.sh
@@ -3,7 +3,7 @@ date_token=`cat /u/scratch/$USER/b1pi_date.txt`
 export TODAYS_DATE=$date_token
 export BMS_OSNAME=`/group/halld/Software/build_scripts/osrelease.pl`
 export BUILD_DIR=/u/scratch/gluex/nightly/$TODAYS_DATE/$BMS_OSNAME
-export SCRIPTS=/group/halld/Software/scripts
+export B1PI_TEST_DIR=/group/halld/Software/scripts/b1pi_test
 export RUN_DIR=/u/scratch/$USER/b1pi/$TODAYS_DATE/$BMS_OSNAME
 
 # Setup environment based on sim-recon build we're using 
@@ -11,14 +11,12 @@ source ${BUILD_DIR}/sim-recon/$BMS_OSNAME/setenv.sh
 export JANA_CALIB_CONTEXT="variation=mc"
 
 # do the test
-rm -rf $RUN_DIR
+rm -rfv $RUN_DIR
 mkdir -pv $RUN_DIR
 cd $RUN_DIR
-cp -pr $SCRIPTS/b1pi_test/* .
-cp -pr $SCRIPTS/b1pi_test/macros/* .
-./mkevents.sh
+./b1pi_test.sh -n 100 -t 4
 export PLOTDIR=/group/halld/www/halldweb/html/b1pi/$TODAYS_DATE/$BMS_OSNAME
 mkdir -pv $PLOTDIR
 cp -v *.pdf *.gif *.html $PLOTDIR
-cp -v ../../../b1pi*.log $PLOTDIR/../
+cp -v /u/scratch/gluex/b1pi*.log $PLOTDIR/../
 exit
