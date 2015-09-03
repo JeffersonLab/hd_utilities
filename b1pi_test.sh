95b75f74f1f5fd816d22a86da3a22adc45949504
marki <marki@e333a0bb-b310-0410-bd06-bbc7ae8e47d2>
2015-09-03 12:34:13 +0000

    - Use default values if input parameters are missing. - Fix setting of the number of threads.

--- b1pi_test.sh
+++ b1pi_test.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-show_help ()
+nevents=1000
+nthreads=1
+vertex="0 0 50 80"
+
+function show_help
 {
-    echo <<EOF
+    cat <<EOF
+Description:
+
+Perform a test of the simulation and reconstruction chain for
+
+    gamma p -> p X
+                 X -> b1 pi-
+                      b1 -> omega pi+
+                            omega -> pi+ pi- pi0
+
 Usage:
-  b1pi_test.sh [-n <number of events>] [-t <number of threads>] -v <vertex string> [-d <b1pi_test script directory>]
+
+  b1pi_test.sh [-n <number of events>] [-t <number of threads>] \\
+    [-v <vertex string>] [-d <b1pi_test script directory>]
+
 Example:
+
+  export B1PI_TEST_DIR /group/halld/Software/scripts/b1pi_test
   b1pi_test.sh -n 1000 -t 4 -v "0 0 50 80"
 EOF
 }
@@ -32,17 +50,35 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
-echo "Leftovers: $@"
+if [ ! -d "$B1PI_TEST_DIR" ]
+    then
+    echo "error: location of scripts and macros not found"
+    echo "       location tried = \"$B1PI_TEST_DIR\""
+    exit 1
+fi
+
+if [ -z "$NEVENTS" ]
+    then
+    echo "info: number of events not defined, using default value $nevents"
+    NEVENTS=$nevents
+fi
+
+if [ -z "$NTHREADS" ]
+    then
+    echo "info: number of threads not defined, using default value $nthreads"
+    NTHREADS=$nthreads
+fi
+
+if [ -z "$VERTEX" ]
+    then
+    echo "info: vertex parameters not defined, using default values $vertex"
+    $VERTEX=$vertex
+fi
+
 echo NEVENTS = $NEVENTS
 echo VERTEX = $VERTEX
 echo NTHREADS = $NTHREADS
 echo B1PI_TEST_DIR = $B1PI_TEST_DIR
-
-if [ -z "$NEVENTS" ]
-    then
-    echo "error: number of events not defined"
-    exit 1
-fi
 
 echo "Copying script files and macros ..."
 cp -pv $B1PI_TEST_DIR/* .
@@ -72,10 +108,10 @@ echo "Running mcsmear ..."
 mcsmear -PJANA:BATCH_MODE=1 hdgeant.hddm
 
 echo "Running hd_root with danarest ..."
-hd_root -PJANA:BATCH_MODE=1 --nthreads=Ncores -PPLUGINS=danarest hdgeant_smeared.hddm
+hd_root -PJANA:BATCH_MODE=1 --nthreads=$NTHREADS -PPLUGINS=danarest hdgeant_smeared.hddm
 
 echo "Running hd_root with b1pi_hists & monitoring_hists ..."
-hd_root -PJANA:BATCH_MODE=1 --nthreads=Ncores -PPLUGINS=b1pi_hists,monitoring_hists dana_rest.hddm
+hd_root -PJANA:BATCH_MODE=1 --nthreads=$NTHREADS -PPLUGINS=b1pi_hists,monitoring_hists dana_rest.hddm
 
 echo "Create plots"
 root -b -q mk_pics.C
