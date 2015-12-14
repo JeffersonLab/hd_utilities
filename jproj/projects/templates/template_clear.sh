f2f72c44fcb982f74cf89f8513c98f819f1c4636
pmatt <pmatt@jlab.org>
2015-12-14 20:32:10 +0000

    Remove monitoring version of jproj: We now use SWIF.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@19862 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- jproj/projects/templates/template_clear.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-# delete the project and start over
-set PROJECT = `basename $PWD`
-echo "clearing tables ${PROJECT}, ${PROJECT}Job.................."
-mysql -hhallddb -ufarmer farming -e "drop table ${PROJECT}, ${PROJECT}Job"
-echo "clearing table ${PROJECT}.................."
-source PROJHOME/../scripts/setup.csh
-jproj.pl ${PROJECT} create
--- jproj/projects/templates/template_fill_jobIds_monAux.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO PROJECT_aux ( jobId)
-SELECT jobId 
-FROM `PROJECTJob`
