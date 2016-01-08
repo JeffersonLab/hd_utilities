41dd0ae124be4c50e3f5770ddae17afb0125c0d9
marki <marki@e333a0bb-b310-0410-bd06-bbc7ae8e47d2>
2016-01-08 23:05:31 +0000

    add redirect to message file

--- b1pi_message.sh
+++ b1pi_message.sh
@@ -5,5 +5,5 @@ rm -f message.txt
 touch message.txt
 date_token=`cat /u/scratch/$USER/b1pi_date.txt`
 echo https://halldweb.jlab.org/b1pi/$date_token/ >> message.txt
-ls /group/halld/www/halldweb/html/b1pi/$date_token/b1pi_*.log | awk ' split($0, t, "b1pi_|.log") { system("echo " $0 ";" "grep \"#count\" " $0)} '
+ls /group/halld/www/halldweb/html/b1pi/$date_token/b1pi_*.log | awk ' split($0, t, "b1pi_|.log") { system("echo " $0 ";" "grep \"#count\" " $0)} ' >> message.txt
 ../../scripts/simple_email_list.pl
