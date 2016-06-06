559b7d9b1e7645a3729b2b1c2b4da57c15c8ffce
pmatt <pmatt@jlab.org>
2016-06-06 12:50:07 +0000

    New launch scripts.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@20837 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

---
+++ launch/launch.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+
+# Paul Mattione
+# Built off of hdswif by Kei Moriya
+#
+# SWIF DOCUMENTATION:
+# https://scicomp.jlab.org/docs/swif
+# https://scicomp.jlab.org/docs/swif-cli
+
+# consider parse_swif & output_job_details scripts
+
+from optparse import OptionParser
+import os.path
+import rcdb
+import sys
+import glob
+from subprocess import Popen, PIPE
+
+VERBOSE = False
+
+####################################################### TRY COMMAND ######################################################
+
+def try_command(command, sleeptime = 5):
+	# Try an os command and if the exit code is non-zero then return an error
+	return_code = -999
+	while return_code != 0:
+		process = Popen(command.split(), stdout=PIPE)
+		output = process.communicate()[0] # is stdout. [1] is stderr
+		print output
+		return_code = process.returncode
+
+		if return_code == 0:
+			break #successful: leave
+
+		# sleep for a few seconds between tries
+		print 'sleeping for ' + str(sleeptime) + ' sec...'
+		time.sleep(sleeptime)
+
+####################################################### READ CONFIG ######################################################
+
+def read_config(CONFIG_FILENAME):
+	# Read in user config file
+	config_dict = {}
+
+	infile_config = open(CONFIG_FILENAME, 'r')
+	for line in infile_config:
+
+		# Ignore empty lines, and lines that begin with #
+		if (len(line.split()) == 0) or (line.split()[0][0] == '#'):
+			continue
+
+		# Add new key/value pair into config_dict
+		key = str(line.split()[0])
+		value = line.split()[1]
+		config_dict[key] = value
+
+	# Some of the keys may depend on other configuration parameters, so update the values
+	# containing [key] within the values corresponding to those keys.
+	#
+	# Example:
+	# OUTPUT_TOPDIR /volatile/halld/test/RunPeriod-[RUNPERIOD]/ver[VERSION]
+	# depends on other config parameters RUNPERIOD and VERSION
+	# 
+	# NOTE: The method assumes there are no circular dependencies
+	  
+	# Iterate over key/value pairs in dictionary. If we find a replacement, we need to start over.
+	# The parameter found keeps track of whether we found a replacement or not.
+	found = 1
+	while(found):
+		for key, value in config_dict.items():
+			found = 0
+
+			# For each one see if any values contain [P] where P is a different value
+			for other_key, other_value in config_dict.items():
+				if str(value).find(str('[' + other_key + ']')) != -1:
+
+					# Found replacement
+					found = 1
+					new_value = value.replace(str('[' + other_key + ']'),other_value)
+
+					# Replace new key/value pair into config_dict
+					new_pair = {key : new_value}
+					config_dict.update(new_pair)
+					del new_pair
+
+					# Break out of loop over other_key, other_value
+					break
+
+			# Break out of loop over key, value, to restart loop again
+			if found == 1:
+				break
+
+		# If we do not find a replacement we will finish the loop
+
+	return config_dict
+
+##################################################### VALIDATE CONFIG ####################################################
+
+def validate_config(config_dict):
+
+	# JOB ACCOUNTING
+	if("PROJECT" not in config_dict) or ("TRACK" not in config_dict) or ("OS" not in config_dict):
+		print "ERROR: JOB ACCOUNTING NOT FULLY SPECIFIED IN CONFIG FILE. ABORTING"
+		sys.exit(1)
+
+	# JOB RESOURCES
+	if("NCORES" not in config_dict) or ("DISK" not in config_dict) or ("RAM" not in config_dict) or ("TIMELIMIT" not in config_dict):
+		print "ERROR: JOB RESOURCES NOT FULLY SPECIFIED IN CONFIG FILE. ABORTING"
+		sys.exit(1)
+
+	# WORKFLOW DEFINITION
+	if("WORKFLOW" not in config_dict):
+		print "ERROR: WORKFLOW DEFINITION NOT FULLY SPECIFIED IN CONFIG FILE. ABORTING"
+		sys.exit(1)
+
+	# JOB, SCRIPT CONTROL
+	if("ENVFILE" not in config_dict) or ("SCRIPTFILE" not in config_dict) or ("JANA_CONFIG" not in config_dict):
+		print "ERROR: JOB, SCRIPT CONTROL NOT FULLY SPECIFIED IN CONFIG FILE. ABORTING"
+		sys.exit(1)
+
+	# FILE INPUT, OUTPUT BASE DIRECTORIES
+	if("INDATA_TOPDIR" not in config_dict) or ("OUTDIR_LARGE" not in config_dict) or ("OUTDIR_SMALL" not in config_dict):
+		print "ERROR: FILE INPUT, OUTPUT BASE DIRECTORIES NOT FULLY SPECIFIED IN CONFIG FILE. ABORTING"
+		sys.exit(1)
+
+####################################################### FIND FILES #######################################################
+
+def find_files(INDATA_DIR, FORMATTED_RUN, FORMATTED_FILE):
+	pathstring = INDATA_DIR + '/*' + FORMATTED_RUN + '*_*' + FORMATTED_FILE + '*'
+	return glob.glob(pathstring)
+
+######################################################## ADD JOB #########################################################
+
+def find_num_threads(JANA_CONFIG_FILENAME):
+	num_threads = "1"
+
+	# Read in JANA config file
+	infile_config = open(JANA_CONFIG_FILENAME, 'r')
+	for line in infile_config:
+
+		# Ignore empty lines, and lines that begin with #
+		if (len(line.split()) == 0) or (line.split()[0][0] == '#'):
+			continue
+
+		# Save #-threads if correct key
+		key = str(line.split()[0])
+		if (key != "NTHREADS"):
+			continue
+		num_threads = line.split()[1]
+		break;
+
+	return num_threads
+
+def add_job(WORKFLOW, FILEPATH, config_dict):
+
+	# BUILD INPUTDATA_TYPE
+	INPUTDATA_TYPE = "mss" if(INDATA_DIR[:5] == "/mss/") else "file"
+
+	# EXTRACT PATH, RUNNO, & FILE #: ASSUME THE FORM */*_RUNNO_FILENO.*
+	match = re.search(r"(.*)/(.*)_(\d\d\d\d\d\d)_(\d\d\d)", FILENAME) # _\d\d\d
+	if (match == ""):
+		print "WARNING: FILE " + INDATA_DIR + "/" + FILENAME + " DOESN'T MATCH EXPECTED NAME FORMAT. SKIPPING."
+		return
+	INDATA_DIR = match.group(1)
+	RUNNO = match.group(3)
+	FILENO = match.group(4)
+
+	# PREPARE NAMES
+	STUBNAME = RUNNO + "_" + FILENO
+	JOBNAME = WORKFLOW + "_" + STUBNAME
+
+	#SETUP OTHER VARIABLES:
+	CACHE_PIN_DAYS = config_dict["CACHE_PIN_DAYS"] if ("CACHE_PIN_DAYS" in config_dict) else "0"
+	JANA_CONFIG = config_dict["JANA_CONFIG"]
+	NUM_THREADS = find_num_threads(JANA_CONFIG)
+
+	# CREATE ADD-JOB COMMAND
+	# job
+	add_command = "swif add-job -workflow " + WORKFLOW + " -name " + JOBNAME
+	# accounting
+	add_command += " -project " + config_dict["PROJECT"] + " -track " + config_dict["TRACK"] + " -os " + config_dict["OS"]
+	# resources
+	add_command += " -cores " + config_dict["NCORES"] + " -disk " + config_dict["DISK"] + " -ram " + config_dict["RAM"] + " -time " + config_dict["TIMELIMIT"]
+	# inputs
+	add_command += " -input " + FILENAME + " " + INPUTDATA_TYPE + ":" + INDATA_DIR + "/" + FILENAME
+	# stdout
+	add_command += " -stdout " + config_dict["OUTDIR_SMALL"] + "/log/" + RUNNO + "/stdout." + STUBNAME + ".out"
+	# stderr
+	add_command += " -stderr " + config_dict["OUTDIR_SMALL"] + "/log/" + RUNNO + "/stderr." + STUBNAME + ".err"
+	# tags
+	add_command += " -tag run_number " + RUNNO + " -tag file_number " + FILENO + " -tag num_threads " + NUM_THREADS
+	# command + arguments
+	add_command += " " + config_dict["SCRIPTFILE"] + " " + config_dict["ENVFILE"] + " " + FILENAME + " " + JANA_CONFIG
+	# command arguments continued
+	add_command += " " + config_dict["OUTDIR_LARGE"] + " " + config_dict["OUTDIR_SMALL"] + " " + RUNNO + " " + FILENO + " " + CACHE_PIN_DAYS
+
+	if(VERBOSE == True):
+		print "job add command is \n" + str(add_command)
+
+	# ADD JOB
+	status = try_command(add_command)
+
+########################################################## MAIN ##########################################################
+
+def main(argv):
+	global VERBOSE # so can modify here
+
+	# PARSER
+	parser_usage = "launch.py job_configfile minrun maxrun\n\n"
+	parser_usage += "optional: -f file_num: file_num must be 3 digits, with leading 0's if necessary)\n"
+	parser_usage += "          but, it can be a search string for glob (e.g. first 5 files: -f '00[0-4]' (MUST include quotes!))\n\n"
+	parser_usage += "optional: -v: verbose output\n\n"
+	parser = OptionParser(usage = parser_usage)
+
+	# PARSER OPTIONS
+	parser.add_option("-f", "--file", dest="file", help="specify file(s) to run over")
+	parser.add_option("-v", "--verbose", dest="verbose", help="verbose output")
+
+	# GET ARGUMENTS
+	(options, args) = parser.parse_args(argv)
+	if(len(args) < 3):
+		parser.print_help()
+		return
+
+	# SET INPUT VARIABLES
+	JOB_CONFIG_FILE = args[0]
+	MINRUN = int(args[1])
+	MAXRUN = int(args[2])
+	VERBOSE = True if(options.verbose) else False
+	INPUT_FILE_NUM = options.file if(options.file) else "*" #must be three digits, with leading 0's if necessary
+
+	# READ CONFIG
+	config_dict = read_config.main(JOB_CONFIG_FILE)
+	validate_config(config_dict)
+
+	# SET CONTROL VARIABLES
+	WORKFLOW = config_dict["WORKFLOW"]
+	RCDB_QUERY = config_dict["RCDB_QUERY"] if ("RCDB_QUERY" in config_dict) else ""
+	INDATA_TOPDIR = config_dict["INDATA_TOPDIR"] if ("INDATA_TOPDIR" in config_dict) else ""
+
+	# GET THE LIST OF GOOD RUNS
+	db = rcdb.RCDBProvider("mysql://rcdb@hallddb/rcdb")
+	good_runs = db.select_runs(RCDB_QUERY, MINRUN, MAXRUN) if(RCDB_QUERY != "") else []
+
+	# FIND & ADD JOBS
+	for RUN in range(MINRUN, MAXRUN + 1):
+		if(RCDB_QUERY != "") and (RUN not in good_runs):
+			continue
+
+		# Format run and file numbers
+		FORMATTED_RUN = "%06d" % RUN
+
+		# Find files for run number
+		INDATA_DIR = INDATA_TOPDIR + "/*" + FORMATTED_RUN + "*/"
+		file_list = find_files(INDATA_DIR, FORMATTED_RUN, INPUT_FILE_NUM)
+		if(VERBOSE == True):
+			print str(len(file_list)) + " files found for run " + RUN
+
+		# Add jobs to workflow
+		for FILEPATH in file_list:
+			add_job(WORKFLOW, FILEPATH, config_dict)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
---
+++ launch/script.sh
@@ -0,0 +1,307 @@
+#!/bin/bash -f
+
+############################################################# SETUP #############################################################
+
+Setup_Script()
+{
+	# PWD, STATUS OF MOUNTED DISKS
+	echo "pwd =" $PWD
+	echo "df -h:"
+	df -h
+
+	# ENVIRONMENT
+	source $ENVIRONMENT
+	printenv
+	echo "PERL INCLUDES: "
+	perl -e "print qq(@INC)"
+
+	# COPY INPUT FILE TO WORKING DIRECTORY
+	# This step is necessary since the cache files will be created as soft links in the current directory, and we want to avoid large I/O processes.
+	# We first copy the input file to the current directory, then remove the link.
+	echo "LOCAL FILES PRIOR TO INPUT COPY"
+	ls -l
+	cp $INPUTFILE ./tmp_file
+	rm -f $INPUTFILE
+	mv tmp_file $INPUTFILE
+	echo "LOCAL FILES AFTER INPUT COPY"
+	ls -l
+}
+
+####################################################### UTILITY FUNCTIONS #######################################################
+
+Extract_SkimName()
+{
+	# to extract the skim name, first extract the locations of the last two periods in the file name
+	local LAST_INDEX=0
+	local SECOND_TO_LAST_INDEX=0
+	local INPUT_FILE=$1
+	for INDEX in `echo $INPUT_FILE | grep -bo '\.' | awk 'BEGIN {FS=":"}{print $1}'`; do
+		SECOND_TO_LAST_INDEX=$LAST_INDEX
+		LAST_INDEX=$INDEX
+	done
+
+	# extract the skim name: awk & grep use different location #'ing, so must convert
+	local LENGTH=$[$LAST_INDEX - $SECOND_TO_LAST_INDEX - 1]
+	local START=$[$SECOND_TO_LAST_INDEX + 2]
+	local SKIM_NAME=`echo $INPUT_FILE | awk -v size="$LENGTH" -v start="$START" '{print substr($0,start,size)}'`
+	echo "SKIM_NAME:" $SKIM_NAME
+	
+	#return the result "by reference"
+	local __result=$2
+	eval $__result="'$SKIM_NAME'"
+}
+
+Extract_BaseName()
+{
+	# base name is everything before the last period
+	local INPUT_FILE=$1
+	local LENGTH=`echo $INPUT_FILE | awk '{print index($0,".")}'`
+	let LENGTH-=1
+	local BASE_NAME=`echo $INPUT_FILE | awk -v size="$LENGTH" '{print substr($0,1,size)}'`
+	echo "BASE_NAME: " $BASE_NAME
+
+	#return the result "by reference"
+	local __result=$2
+	eval $__result="'$BASE_NAME'"
+}
+
+####################################################### SAVE OUTPUT FILES #######################################################
+
+Save_OutputFiles()
+{
+	# SEE WHAT FILES ARE PRESENT
+	echo "FILES PRESENT PRIOR TO SAVE:"
+	ls -l
+
+	# REMOVE INPUT FILE: so that it's easier to determine which remaining files are skims
+	rm -f $INPUTFILE
+
+	# BUILD TAPEDIR, IF $OUTDIR_LARGE STARTS WITH "/cache/"  
+	# If so, output files are pinned & jput.  If not, then they aren't. 
+	local TAPEDIR=""
+	local OUTDIR_LARGE_BASE=`echo $OUTDIR_LARGE | awk '{print substr($0,1,7)}'`
+	# first strip /cache/, then insert /mss/
+	if [ "$OUTDIR_LARGE_BASE" == "/cache/" ]; then
+		local OUTPATH=`echo $OUTDIR_LARGE | awk '{print substr($0,8)}'`
+		TAPEDIR=/mss/${OUTPATH}/
+	fi
+
+	# CALL SAVE FUNCTIONS
+	Save_Histograms
+	Save_REST
+	Save_JANADot
+	Save_EVIOSkims
+	Save_HDDMSkims
+	Save_ROOTFiles
+	Save_IDXA
+
+	# SEE WHAT FILES ARE LEFT
+	echo "FILES REMAINING AFTER SAVING:"
+	ls -l
+}
+
+Save_Histograms()
+{
+	# SAVE ROOT HISTOGRAMS
+	if [ -e hd_root.root ]; then
+		echo "Saving histogram file"
+
+		# setup output dirs
+		local OUTDIR_THIS=${OUTDIR_LARGE}/hists/${RUN_NUMBER}/
+		local TAPEDIR_THIS=${TAPEDIR}/hists/${RUN_NUMBER}/
+		mkdir -p -m 775 ${OUTDIR_THIS}
+
+		# save it
+		local OUTPUT_FILE=${OUTDIR_THIS}/hd_root_${RUN_NUMBER}_${FILE_NUMBER}.root
+		mv -v hd_root.root $OUTPUT_FILE
+		chmod 664 $OUTPUT_FILE
+
+		# force save to tape & pin
+		if [ "$TAPEDIR" != "" ]; then
+			jcache pin $OUTPUT_FILE -D $CACHE_PIN_DAYS
+			jput $OUTPUT_FILE $TAPEDIR_THIS/
+		fi
+	fi
+}
+
+Save_REST()
+{
+	# SAVE REST FILE
+	if [ -e dana_rest.hddm ]; then
+		echo "Saving REST file"
+
+		# setup output dirs
+		local OUTDIR_THIS=${OUTDIR_LARGE}/REST/${RUN_NUMBER}/
+		local TAPEDIR_THIS=${TAPEDIR}/REST/${RUN_NUMBER}/
+		mkdir -p -m 775 $OUTDIR_THIS
+
+		# save it
+		local OUTPUT_FILE=${OUTDIR_THIS}/dana_rest_${RUN_NUMBER}_${FILE_NUMBER}.hddm
+		mv -v dana_rest.hddm $OUTPUT_FILE
+		chmod 664 $OUTPUT_FILE
+
+		# force save to tape & pin
+		if [ "$TAPEDIR" != "" ]; then
+			jcache pin $OUTPUT_FILE -D $CACHE_PIN_DAYS
+			jput $OUTPUT_FILE $TAPEDIR_THIS/
+		fi
+	fi
+}
+
+Save_JANADot()
+{
+	# SAVE JANADOT FILE
+	if [ -e jana.dot ]; then
+		echo "Saving JANADOT file"
+		dot -Tps2 jana.dot -o jana.ps
+		ps2pdf jana.ps
+
+		# setup output dir
+		local OUTDIR_THIS=${OUTDIR_SMALL}/log/${RUN_NUMBER}/
+		mkdir -p -m 775 $OUTDIR_THIS
+
+		# save it
+		local OUTPUT_FILE=${OUTDIR_THIS}/janadot_${RUN_NUMBER}_${FILE_NUMBER}.pdf
+		mv -v jana.pdf $OUTPUT_FILE
+		chmod 664 $OUTPUT_FILE
+	fi
+}
+
+Save_EVIOSkims()
+{
+	# SAVE EVIO SKIMS
+	echo "Saving EVIO skim files (if any)"
+	for EVIO_FILE in `find . -type -f -name "*.evio"`; do
+		Extract_SkimName $EVIO_FILE SKIM_NAME
+
+		# setup output dir
+		local OUTDIR_THIS=${OUTDIR_LARGE}/${SKIM_NAME}/${RUN_NUMBER}/
+		local TAPEDIR_THIS=${TAPEDIR}/${SKIM_NAME}/${RUN_NUMBER}/
+		mkdir -p -m 775 $OUTDIR_THIS
+
+		# save it
+		local OUTPUT_FILE=${OUTDIR_THIS}/${EVIO_FILE}
+		mv -v $EVIO_FILE $OUTPUT_FILE
+		chmod 664 $OUTPUT_FILE
+
+		# force save to tape & pin
+		if [ "$TAPEDIR" != "" ]; then
+			jcache pin $OUTPUT_FILE -D $CACHE_PIN_DAYS
+			jput $OUTPUT_FILE $TAPEDIR_THIS/
+		fi
+	end
+}
+
+Save_HDDMSkims()
+{
+	# SAVE HDDM SKIMS #assumes REST file already backed up and removed!
+	echo "Saving HDDM skim files (if any)"
+	for HDDM_FILE in `find . -type -f -name "*.hddm"`; do
+		Extract_SkimName $HDDM_FILE SKIM_NAME
+
+		# setup output dir
+		local OUTDIR_THIS=${OUTDIR_LARGE}/${SKIM_NAME}/${RUN_NUMBER}/
+		local TAPEDIR_THIS=${TAPEDIR}/${SKIM_NAME}/${RUN_NUMBER}/
+		mkdir -p -m 775 $OUTDIR_THIS
+
+		# save it
+		local OUTPUT_FILE=${OUTDIR_THIS}/${HDDM_FILE}
+		mv -v $HDDM_FILE $OUTPUT_FILE
+		chmod 664 $OUTPUT_FILE
+
+		# force save to tape & pin
+		if [ "$TAPEDIR" != "" ]; then
+			jcache pin $OUTPUT_FILE -D $CACHE_PIN_DAYS
+			jput $OUTPUT_FILE $TAPEDIR_THIS/
+		fi
+	end
+}
+
+Save_ROOTFiles()
+{
+	# SAVE OTHER ROOT FILES
+	echo "Saving other ROOT files (if any)"
+	for ROOT_FILE in `find . -type -f -name "*.root"`; do
+		Extract_BaseName $ROOT_FILE BASE_NAME
+
+		# setup output dir
+		local OUTDIR_THIS=${OUTDIR_LARGE}/${BASE_NAME}/${RUN_NUMBER}/
+		local TAPEDIR_THIS=${TAPEDIR}/${BASE_NAME}/${RUN_NUMBER}/
+		mkdir -p -m 775 $OUTDIR_THIS
+
+		# save it
+		local OUTPUT_FILE=${OUTDIR_THIS}/${BASE_NAME}_${RUN_NUMBER}_${FILE_NUMBER}.root
+		mv -v $ROOT_FILE $OUTPUT_FILE
+		chmod 664 $OUTPUT_FILE
+
+		# force save to tape & pin
+		if [ "$TAPEDIR" != "" ]; then
+			jcache pin $OUTPUT_FILE -D $CACHE_PIN_DAYS
+			jput $OUTPUT_FILE $TAPEDIR_THIS/
+		fi
+	end
+}
+
+Save_IDXA()
+{
+	# SAVE IDXA FILES
+	echo "Saving IDXA files (if any)"
+	for IDXA_FILE in `find . -type -f -name "*.idxa"`; do
+		Extract_BaseName $IDXA_FILE BASE_NAME
+
+		# setup output dir
+		local OUTDIR_THIS=${OUTDIR_SMALL}/IDXA/${RUN_NUMBER}/
+		mkdir -p -m 775 $OUTDIR_THIS
+
+		# save it
+		local OUTPUT_FILE=${OUTDIR_THIS}/${BASE_NAME}_${RUN_NUMBER}_${FILE_NUMBER}.idxa
+		mv -v $IDXA_FILE $OUTPUT_FILE
+		chmod 664 $OUTPUT_FILE
+	end
+}
+
+########################################################## MAIN FUNCTION ########################################################
+
+Run_Script()
+{
+	Setup_Script
+
+	# RUN JANA
+	hd_root $INPUTFILE --config=$CONFIG_FILE
+
+	# RETURN CODE
+	RETURN_CODE=$?
+	echo "Return Code = " $RETURN_CODE
+	if [ $RETURN_CODE -ne 0 ]; then
+		exit $RETURN_CODE
+	fi
+
+	# SAVE OUTPUTS
+	Save_OutputFiles
+}
+
+######################################################### EXECUTE SCRIPT ########################################################
+
+# SET INPUTS
+ENVIRONMENT=$1
+INPUTFILE=$2
+CONFIG_FILE=$3
+OUTDIR_LARGE=$4
+OUTDIR_SMALL=$5
+RUN_NUMBER=$6
+FILE_NUMBER=$7
+CACHE_PIN_DAYS=$8
+
+# PRINT INPUTS
+echo "ENVIRONMENT       = $ENVIRONMENT"
+echo "INPUTFILE         = $INPUTFILE"
+echo "CONFIG_FILE       = $CONFIG_FILE"
+echo "OUTDIR_LARGE      = $OUTDIR_LARGE"
+echo "OUTDIR_SMALL      = $OUTDIR_SMALL"
+echo "RUN_NUMBER        = $RUN_NUMBER"
+echo "FILE_NUMBER       = $FILE_NUMBER"
+echo "CACHE_PIN_DAYS    = $CACHE_PIN_DAYS"
+
+# RUN
+Run_Script
+
