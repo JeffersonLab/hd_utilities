c5e7d2d1432b4e1006a21961acaddbad7e645959
pmatt <pmatt@jlab.org>
2016-09-29 13:39:54 +0000

    Look for files on cache disk.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@21396 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- incoming/cron_exec.sh
+++ incoming/cron_exec.sh
@@ -2,5 +2,5 @@
 
 #rm -f ~/incoming_log.txt
 source ~/env_monitoring_incoming.sh #> ~/incoming_log.txt
-python ~/monitoring/incoming/submit_jobs.py 2016-10 ~/monitoring/incoming/input.config 5 #>> ~/incoming_log.txt
+python ~/monitoring/incoming/submit_jobs.py 2016-10 ~/monitoring/incoming/jobs_incoming.config 5 #>> ~/incoming_log.txt
 
