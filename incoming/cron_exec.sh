8a6e5c03011b9ed3c87a54539c94b39a700194f8
pmatt <pmatt@jlab.org>
2016-09-29 15:08:36 +0000

    Trying to make it work.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@21398 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- incoming/cron_exec.sh
+++ incoming/cron_exec.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#rm -f ~/incoming_log.txt
-source ~/env_monitoring_incoming.sh #> ~/incoming_log.txt
-python ~/monitoring/incoming/submit_jobs.py 2016-10 ~/monitoring/incoming/jobs_incoming.config 5 #>> ~/incoming_log.txt
+source ~/env_monitoring_incoming.sh
+python ~/monitoring/incoming/submit_jobs.py 2016-10 ~/monitoring/incoming/jobs_incoming.config 5
 
