f7894782ef2f2ce229f3472110643b978c395e87
pmatt <pmatt@jlab.org>
2016-09-30 18:25:51 +0000

    Not sure. Fixes or improvements, hopefully.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@21402 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- incoming/cron_exec.sh
+++ incoming/cron_exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source ~/env_monitoring_incoming.sh
-export PATH=/site/bin:${PATH}
+export PATH=/site/bin:${PATH} #because .login isn't executed, and need this path for SWIF
 python ~/monitoring/incoming/submit_jobs.py 2016-10 ~/monitoring/incoming/jobs_incoming.config 5
 
