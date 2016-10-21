9c6199dae533cc2e76629497155ce9545830909b
pmatt <pmatt@jlab.org>
2016-10-21 14:50:38 +0000

    Fix environment script.
    git-svn-id: https://halldsvn.jlab.org/repos/trunk/scripts/monitoring@21507 e333a0bb-b310-0410-bd06-bbc7ae8e47d2

--- recon_test/env_recon_test.sh
+++ recon_test/env_recon_test.sh
@@ -2,13 +2,6 @@
 
 export MONITORING_HOME=/group/halld/Software/scripts/monitoring/
 
-# SET /apps/bin AT FRONT OF PATH (to use working version of git)
-export PATH=/apps/bin:$PATH
-
-# PYTHON
-export PATH=/apps/python/PRO/bin:$PATH
-export LD_LIBRARY_PATH=/apps/python/PRO/lib:$LD_LIBRARY_PATH
-
 # SET PATH FOR PERL MODULES (Does not seem to be automatically present when running jobs from cron)
 export PERL5LIB=/usr/local/lib64/perl5:/usr/local/share/perl5:/usr/lib64/perl5/vendor_perl:/usr/share/perl5/vendor_perl:/usr/lib64/perl5:/usr/share/perl5
 
@@ -21,10 +14,9 @@ export EDITOR=vim
 export GLUEX_TOP=/group/halld/Software/builds/$BMS_OSNAME/
 export DATE=`date +%Y-%m-%d`
 export GLUEX_VERSION_XML=/u/scratch/gluex/nightly/${DATE}/${BMS_OSNAME}/version_${DATE}.xml
-eval `$BUILD_SCRIPTS/version.pl -s bash $GLUEX_VERSION_XML`
 
 # SET FULL ENVIRONMENT
-source $BUILD_SCRIPTS/gluex_env_jlab.sh
+source $BUILD_SCRIPTS/gluex_env_jlab.sh $GLUEX_VERSION_XML
 
 # SETUP SQLITE & RESOURCES
 export SQLITE_PATH=ccdb.sqlite #only used locally
