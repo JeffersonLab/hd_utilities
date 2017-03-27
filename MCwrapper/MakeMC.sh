2de12f9fca257fb189a601209da88d79238e675e
tbritton <tbritton@jlab.org>
2017-03-27 11:27:46 -0400

    indentation fixes

--- MCwrapper/MakeMC.sh
+++ MCwrapper/MakeMC.sh
@@ -54,7 +54,7 @@ shift
 export GEN_MAX_ENERGY=$1
 
 if [[ "$GEANTVER" == "3" ]]; then
-export NUMTHREADS=1
+    export NUMTHREADS=1
 fi
 
 # PRINT INPUTS
@@ -76,18 +76,18 @@ echo "detected bash"
 #printenv
 #necessary to run swif, uses local directory if swif=0 is used
 if [[ "$MCSWIF" -eq "1" ]]; then
-# ENVIRONMENT
-echo $ENVIRONMENT
-source $ENVIRONMENT
-echo pwd = $PWD
-mkdir -p $OUTDIR
-mkdir -p $OUTDIR/log
+    # ENVIRONMENT
+    echo $ENVIRONMENT
+    source $ENVIRONMENT
+    echo pwd = $PWD
+    mkdir -p $OUTDIR
+    mkdir -p $OUTDIR/log
 fi
 
 if [[ "$CUSTOM_GCONTROL" -eq "0"  ]]; then
-cp $MCWRAPPER_CENTRAL/Gcontrol.in ./
+    cp $MCWRAPPER_CENTRAL/Gcontrol.in ./
 else
-cp $CUSTOM_GCONTROL/Gcontrol.in ./
+    cp $CUSTOM_GCONTROL/Gcontrol.in ./
 fi
 
 if [[ "$GENR" != "0" ]]; then
@@ -96,14 +96,14 @@ if [[ "$GENR" != "0" ]]; then
 	echo "only [genr8, bggen, genEtaRegge, gen_2pi_amp, gen_pi0] are supported"
 	exit
     fi
-
+    
     if [[ -f $CONFIG_FILE  ]]; then
-	    echo " input file found"
-	else
-	    echo $CONFIG_FILE" does not exist"
-	    exit
+	echo " input file found"
+    else
+	echo $CONFIG_FILE" does not exist"
+	exit
     fi
-
+    
     if [[ "$GENERATOR" -eq "genr8" ]]; then
 	echo "configuring genr8"
 	cp $CONFIG_FILE ./genr8\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf
@@ -152,7 +152,7 @@ if [[ "$GENR" != "0" ]]; then
     else if [[ "$GENERATOR" -eq "bggen" ]]; then
 	set colsize=`rcnd $RUN_NUMBER collimator_diameter | awk '{print $1}' | sed -r 's/.{2}$//' | sed -e 's/\.//g'`
 	if [[ "$colsize" -eq "B" || "$colsize" -eq "R"  ]]; then
-	set colsize = "34"
+	    set colsize = "34"
 	fi
 	set RANDOM=$$
 	echo $RANDOM
@@ -168,11 +168,11 @@ if [[ "$GENR" != "0" ]]; then
 	ln -s bggen\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf fort.15
 	bggen
 	mv bggen.hddm $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm
-        else if [[ "$GENERATOR" -eq "genEtaRegge" ]]; then
+    else if [[ "$GENERATOR" -eq "genEtaRegge" ]]; then
 	echo "RUNNING GENETAREGGE" 
 	set colsize=`rcnd $RUN_NUMBER collimator_diameter | awk '{print $1}' | sed -r 's/.{2}$//' | sed -e 's/\.//g'`
 	if [[ "$colsize" -eq "B" || "$colsize" -eq "R"  ]]; then
-	set colsize = "34"
+	    set colsize = "34"
 	fi
 	sed -i 's/TEMPCOLD/'0.00$colsize'/' genEtaRegge\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf
 	sed -i 's/TEMPELECE/'$eBEAM_ENERGY'/' genEtaRegge\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf
@@ -180,35 +180,35 @@ if [[ "$GENR" != "0" ]]; then
 	sed -i 's/TEMPMINGENE/'$GEN_MIN_ENERGY'/' genEtaRegge\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf
 	sed -i 's/TEMPMAXGENE/'$GEN_MAX_ENERGY'/' genEtaRegge\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf
 	genEtaRegge -N$EVT_TO_GEN -O$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm -I'genEtaRegge'\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf
-	else if [[ "$GENERATOR" -eq "gen_2pi_amp" ]]; then
+    else if [[ "$GENERATOR" -eq "gen_2pi_amp" ]]; then
 	echo "RUNNING GEN_2PI_AMP" 
         set optionals_line = `head -n 1 $config_file_name | sed -r 's/.//'`
 	echo $optionals_line
 	echo gen_2pi_amp -c gen_2pi_amp\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf -o $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm -hd $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.root -n $EVT_TO_GEN -r $RUN_NUMBER  -a $GEN_MIN_ENERGY -b $GEN_MAX_ENERGY $optionals_line
 	gen_2pi_amp -c gen_2pi_amp\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf -hd $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm -o $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.root -n $EVT_TO_GEN -r $RUN_NUMBER -a $GEN_MIN_ENERGY - b $GEN_MAX_ENERGY $optionals_line
-	else if [[ "$GENERATOR" -eq "gen_2pi_primakoff" ]]; then
+    else if [[ "$GENERATOR" -eq "gen_2pi_primakoff" ]]; then
 	echo "RUNNING GEN_2PI_PRIMAKOFF" 
         set optionals_line = `head -n 1 $config_file_name | sed -r 's/.//'`
 	echo $optionals_line
 	echo gen_2pi_primakoff -c gen_2pi_primakoff\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf -o $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm -hd $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.root -n $EVT_TO_GEN -r $RUN_NUMBER  -a $GEN_MIN_ENERGY -b $GEN_MAX_ENERGY $optionals_line
 	gen_2pi_primakoff -c gen_2pi_primakoff\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf -hd $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm -o $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.root -n $EVT_TO_GEN -r $RUN_NUMBER -a $GEN_MIN_ENERGY - b $GEN_MAX_ENERGY $optionals_line
-	else if [[ "$GENERATOR" -eq "gen_pi0" ]]; then
+    else if [[ "$GENERATOR" -eq "gen_pi0" ]]; then
 	echo "RUNNING GEN_PI0" 
         set optionals_line = `head -n 1 $config_file_name | sed -r 's/.//'`
 	echo $optionals_line
 	gen_pi0 -c gen_pi0\_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.conf -hd $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm -o $GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.root -n $EVT_TO_GEN -r $RUN_NUMBER -a $GEN_MIN_ENERGY -b $GEN_MAX_ENERGY -p $COHERENT_PEAK  -s $FILE_NUMBER $optionals_line -m $eBEAM_ENERGY
     fi
-
-#GEANT/smearing
-#modify TEMPIN/TEMPOUT/TEMPTRIG/TOSMEAR in control.in
-
+    
+    #GEANT/smearing
+    #modify TEMPIN/TEMPOUT/TEMPTRIG/TOSMEAR in control.in
+    
     if [[ "$GEANT" != "0" ]]; then
 	echo "RUNNING GEANT"$GEANTVER
 	set colsize=`rcnd $RUN_NUMBER collimator_diameter | awk '{print $1}' | sed -r 's/.{2}$//' | sed -e 's/\.//g'`
 	if [[ "$colsize" -eq "B" || "$colsize" -eq "R"  ]]; then
-	set colsize = "34"
+	    set colsize = "34"
 	fi
-
+	
 	set inputfile=$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER
 	cp Gcontrol.in $PWD/control'_'$RUN_NUMBER'_'$FILE_NUMBER.in
 	sed -i 's/TEMPIN/'$inputfile.hddm'/' control'_'$RUN_NUMBER'_'$FILE_NUMBER.in
@@ -220,27 +220,27 @@ if [[ "$GENR" != "0" ]]; then
 	mv $PWD/control'_'$RUN_NUMBER'_'$FILE_NUMBER.in $PWD/control.in
 	
 	if [[ "$GEANTVER" -eq "3" ]]; then
-	hdgeant 
+	    hdgeant 
 	else if [[ "$GEANTVER" -eq "4" ]]; then
-	#make run.mac then call it below
-	rm run.mac
-	echo /run/beamOn $EVT_TO_GEN >> run.mac
-	echo "exit" >> run.mac
-	hdgeant4 -t$NUMTHREADS run.mac
-	rm run.mac
+	    #make run.mac then call it below
+	    rm run.mac
+	    echo /run/beamOn $EVT_TO_GEN >> run.mac
+	    echo "exit" >> run.mac
+	    hdgeant4 -t$NUMTHREADS run.mac
+	    rm run.mac
 	else
-	echo "INVALID GEANT VERSION"
-	exit
+	    echo "INVALID GEANT VERSION"
+	    exit
 	fi
 	
 	if [[ "$SMEAR" != "0" ]]; then
 	    echo "RUNNING MCSMEAR"
 	    
 	    if [[ "$BKGFOLDSTR" -eq "0" ]]; then
-	    mcsmear -o$inputfile'_geant_smeared.hddm' $inputfile'_geant.hddm'
+		mcsmear -o$inputfile'_geant_smeared.hddm' $inputfile'_geant.hddm'
 	    else
-	    echo 'mcsmear -o'$inputfile'_geant_smeared.hddm'' '$inputfile'_geant.hddm'' '$BKGFOLDSTR
-	    mcsmear -o$inputfile'_geant_smeared.hddm' $inputfile'_geant.hddm' $BKGFOLDSTR
+		echo 'mcsmear -o'$inputfile'_geant_smeared.hddm'' '$inputfile'_geant.hddm'' '$BKGFOLDSTR
+		mcsmear -o$inputfile'_geant_smeared.hddm' $inputfile'_geant.hddm' $BKGFOLDSTR
 	    fi
 	    #run reconstruction
 	    if [[ "$CLEANGENR" -eq "1" ]]; then
@@ -263,18 +263,18 @@ if [[ "$GENR" != "0" ]]; then
 		mv dana_rest.hddm dana_rest_$GEN_NAME\_$RUN_NUMBER\_$FILE_NUMBER.hddm
 		
 		if [[ "$CLEANGEANT" -eq "1" ]]; then
-		rm *_geant.hddm
-		rm Gcontrol.in
-		
+		    rm *_geant.hddm
+		    rm Gcontrol.in
+		    
 		fi
 		
 		if [[ "$CLEANSMEAR" -eq "1" ]]; then
-		rm *_smeared.hddm
-		rm smear.root
+		    rm *_smeared.hddm
+		    rm smear.root
 		fi
 		
 		if [[ "$CLEANRECON" -eq "1" ]]; then
-		rm dana_rest*
+		    rm dana_rest*
 		fi
 	    fi
 	fi
@@ -293,6 +293,6 @@ fi
 if [[ ! -d "$OUTDIR/root/" ]]; then
     mkdir $OUTDIR/root/
 fi
-    mv $PWD/*.conf $OUTDIR/configurations/
-    mv $PWD/*.hddm $OUTDIR/hddm/
-    mv $PWD/*.root $OUTDIR/root/
+mv $PWD/*.conf $OUTDIR/configurations/
+mv $PWD/*.hddm $OUTDIR/hddm/
+mv $PWD/*.root $OUTDIR/root/
