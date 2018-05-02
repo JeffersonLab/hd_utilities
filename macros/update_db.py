5882b935f810981d7648709e258431ab42344e7e
sdobbs <sdobbs@e333a0bb-b310-0410-bd06-bbc7ae8e47d2>
2018-05-02 18:22:00 +0000

    Add in some default config for testing

--- macros/update_db.py
+++ macros/update_db.py
@@ -33,7 +33,10 @@ with open("data.txt") as f:
 db_conn = MySQLdb.connect("hallddb.jlab.org", "datmon", "", "data_monitoring")
 db = db_conn.cursor()
 
-db.execute("INSERT INTO b1pi_test_data (platform,date_generated,run,recon_pi0,recon_omega,recon_b1,recon_b1pi,gen_photons,gen_protons,gen_pip,gen_pim,recon_photons,recon_protons,recon_pip,recon_pim,config,comment)  VALUES ('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\"%s\",\"%s\")"%(bms_osname,datestr,run,vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6],vals[7],vals[8],vals[9],vals[10],vals[11],"",""))
+# put something into the display configuration for testing
+config = '{ "recon_pi0": {"lowLimit": 990, "highLimit": 1020, "bounding": "typical_hi"} }'
+
+db.execute("INSERT INTO b1pi_test_data (platform,date_generated,run,recon_pi0,recon_omega,recon_b1,recon_b1pi,gen_photons,gen_protons,gen_pip,gen_pim,recon_photons,recon_protons,recon_pip,recon_pim,config,comment)  VALUES ('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\"%s\",\"%s\")"%(bms_osname,datestr,run,vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6],vals[7],vals[8],vals[9],vals[10],vals[11],config,""))
 
 db_conn.commit()
 db_conn.close()
