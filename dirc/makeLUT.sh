978e498b656cb7fe8945cc53729747c18e7db8ff
Justin Stevens <jrsteven@jlab.org>
2018-12-26 15:23:47 -0500

    add script to make all LUT with one command

---
+++ dirc/makeLUT.sh
@@ -0,0 +1,15 @@
+
+ln -s $HD_UTILITIES_HOME/dirc/loadlib.C
+ln -s $HD_UTILITIES_HOME/dirc/glxlut_add.C
+ln -s $HD_UTILITIES_HOME/dirc/glxlut_avr.C
+ln -s $HD_UTILITIES_HOME/dirc/glxlut_convert.C
+
+$HD_UTILITIES_HOME/dirc/loop_lut_dirc.py
+$HD_UTILITIES_HOME/dirc/loop_lut_avr.py
+
+root -b -q loadlib.C glxlut_add.C+'("tree/lut_avr_*.root")'
+root -b -q loadlib.C glxlut_convert.C
+
+rm AutoDict_vector_TVector3*
+rm glxlut_add_C*
+rm glxlut_avr_C*
