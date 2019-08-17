78b903e5c83bc451a0b66b8ae8fd61c7b6f2095b
Mark Ito <marki@jlab.org>
2019-08-17 11:24:55 -0400

    refine cache report

--- disk_management/cache_report.sh
+++ disk_management/cache_report.sh
@@ -13,9 +13,15 @@ EOF
 date >> $report_file
 cat >> $report_file <<EOF
 </p>
-<p>Oldest 1000 files under /cache/halld</p>
+<p>Total size (GB) of unpinned raw data files under /cache/halld</p>
 EOF
-query="select file.mod_time as mtime, file_name, file.owner, size, full_path from file, directory where file.dir_index = directory.dir_index and full_path like \"/cache/halld/%\" and isCached = 1 order by mtime limit 1000;"
+query="select sum(size)/1024/1024/1024 as size_GB from file, directory, projectDisk where file.dir_index = directory.dir_index and projectDisk.disk_index = directory.disk_index and isCached = 1 and isTaped = 1 and root = \"/cache/halld\" and file.owner = \"halldata\" and file_index not in (select file_index from pin) order by file.mod_time;"
+mysql --html -h scidbw -u dummy wdm -e "$query" >> $report_file
+cat >> $report_file <<EOF
+</p>
+<p>Oldest unpinned, backed up files under /cache/halld</p>
+EOF
+query="select file.mod_time as mtime, file_name, file.owner, size, full_path from file, directory, projectDisk where file.dir_index = directory.dir_index and projectDisk.disk_index = directory.disk_index and isCached = 1 and isTaped = 1 and root = \"/cache/halld\" and file.owner != \"halldata\" and file_index not in (select file_index from pin) order by mtime limit 100;"
 mysql --html -h scidbw -u dummy wdm -e "$query" >> $report_file
 cat >> $report_file <<EOF
 </body>
