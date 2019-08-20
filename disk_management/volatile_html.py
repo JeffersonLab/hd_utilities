f16ed59159191f51c9db07413aa24c95e737ce75
Mark Ito <marki@jlab.org>
2019-08-20 13:58:16 -0400

    Make the volatile report with python script.

---
+++ disk_management/volatile_html.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import mysql.connector
+import time
+
+now = time.strftime('%c')
+datetime = str(now)
+
+db = mysql.connector.connect(
+  host="scidbw.jlab.org",
+  user="dummy",
+  passwd="",
+  database="wdm"
+)
+
+dirlist = []
+
+def checkdir(dir):
+  if dir not in dirlist:
+    dirlist.append(dir)
+    return True
+  else:
+    return False
+
+print('<html>')
+print('<head><title>Oldest Volatile Files</title></head>')
+print('<body>')
+print('<p>' + datetime + '</p>')
+print('<table border>')
+print('<tr><th>running directory count</th><th>running sum of <br/> size of old files (GB)</th><th>directories with oldest files</th><th>running file count</th><th>oldest file in directory</th><th>file mod time</th><th>file owner</th></tr>')
+
+cursor = db.cursor()
+
+cursor.execute('select vfile.mod_time as mtime, file_name, vfile.owner, size, full_path from vfile, vdirectory, projectDisk where vfile.dir_index = vdirectory.dir_index and projectDisk.disk_index = vdirectory.disk_index and root = "/volatile/halld" order by mtime limit 100000')
+
+result = cursor.fetchall()
+
+count = 0
+count_dir = 0
+sum = float(0);
+for i in range(len(result)):
+  count += 1
+  line_array = result[i]
+  sum += float(line_array[3])
+  dir = str(line_array[4])
+  if checkdir(dir):
+    count_dir += 1
+    line_str_array = []
+    for j in range(len(line_array)):
+      line_str_array.append(str(line_array[j]))
+    line = '<tr>' + '<td>' + str(count_dir) + '</td>' + '<td>' + str(sum/1024.0/1024.0/1024.0) + '</td>' + '<td>' + line_str_array[4] + '</td>' + '<td>' + str(count) + '</td>' + '<td>' + line_str_array[1] + '</td>' + '<td>' + line_str_array[0] + '</td>' + '<td>' + line_str_array[2] + '</td>' + '</tr>'
+    print(line)
+
+print('</table>')
+print('</body>')
+print('</html>')
+
+
--- disk_management/volatile_report.sh
+++ disk_management/volatile_report.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 report_file=/tmp/volatile_oldest.html
 rm -f $report_file
-cat >> $report_file <<EOF
-<html>
-<head>
-<title>Hall D Volatile Disk Report</title>
-</head>
-<body>
-<h1>Hall D Volatile Disk Report</h1>
-<p>
-EOF
-date >> $report_file
-cat >> $report_file <<EOF
-</p>
-<p>Oldest 1000 files under /volatile/halld</p>
-EOF
-query="select vfile.mod_time as mtime, file_name, vfile.owner, size, full_path from vfile, vdirectory where vfile.dir_index = vdirectory.dir_index and full_path like \"/volatile/halld/%\" order by mtime limit 1000;"
-mysql --html -h scidbw -u dummy wdm -e "$query" >> $report_file
-cat >> $report_file <<EOF
-</body>
-</html>
-EOF
+/group/halld/Software/hd_utilities/disk_management/volatile_html.py > $report_file
 mv $report_file /group/halld/www/halldweb/html/disk_management/
