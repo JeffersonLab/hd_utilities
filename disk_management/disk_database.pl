fc985ce013fd0d125dc7900385e9c6189ed0735b
markito3 <marki@jlab.org>
2020-11-20 15:53:39 -0500

    output fewer progress messages

--- disk_management/disk_database.pl
+++ disk_management/disk_database.pl
@@ -86,7 +86,7 @@ open (FINDDIR, $find_dir_command);
 $idebug = 0;
 $jdebug = 0;
 while ($dirname = <FINDDIR>) {
-    if (!($idebug%100)) {print "$idebug: dirname = $dirname";}
+    if (!($idebug%1000)) {print "$idebug: dirname = $dirname";}
     chomp $dirname;
     @stat = stat($dirname);
     $sql = "insert into $dir_table set dirname = \"$dirname\", uid = $stat[4], size = $stat[7];";
@@ -106,7 +106,7 @@ while ($dirname = <FINDDIR>) {
     open (FINDFILE, "find $dirname -maxdepth 1 -type f |");
     while ($filename = <FINDFILE>) {
 	chomp $filename;
-	if (!($jdebug%1000)) {print "$jdebug: $filename\n";}
+	if (!($jdebug%10000)) {print "$jdebug: $filename\n";}
 	@stat = stat($filename);
 	if (@stat) {
 	    @token = split(/\//, $filename);
