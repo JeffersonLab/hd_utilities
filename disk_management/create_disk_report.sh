2ebf28a813f57bf9f020eb14ce62f16667cf2766
markito3 <marki@jlab.org>
2021-01-04 20:15:46 -0500

    unify database entry and report making in bash script

---
+++ disk_management/create_disk_report.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dir=$1
+token=$2
+temp_output_dir=/tmp/disk_report_$RANDOM
+webdir=/group/halld/www/halldweb/html/disk_management
+
+#dmdir=/group/halld/Software/hd_utilities/disk_management
+dmdir=/home/marki/git/hd_utilities/disk_management
+$dmdir/disk_database.pl $dir $token
+mkdir -pv $temp_output_dir
+$dmdir/disk_report.pl $token > $temp_output_dir/${token}_report.html
+$dmdir/disk_report_users.pl $token $temp_output_dir
+
+cp -pv $temp_output_dir/*.html $webdir/
---
+++ disk_management/disk_report_users.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+use DBI;
+use Getopt::Std;
+
+$directory_label = $ARGV[0];
+$output_dir = $ARGV[1];
+$dir_table = $directory_label . "_dir";
+$file_table = $directory_label . "_file";
+$update_time_table = $directory_label . "_updateTime";
+
+$user = "diskmanager";
+$password = "";
+$database = "diskManagement";
+$hostname = "localhost";
+
+$seconds_per_year = 365.25*24*60*60;
+
+$dbh = DBI->connect("DBI:mysql:$database:$hostname", $user, $password);
+
+$sql = "select sum(($file_table.size*1.e-9)*(unix_timestamp(now())-unix_timestamp(atime))) as sumgby, $file_table.uid from $file_table group by $file_table.uid order by sumgby desc limit 10;";
+make_query($dbh, \$sth);
+$i = 1;
+while (@row = $sth->fetchrow_array) {
+    $age_size = round($row[0]/$seconds_per_year);
+    $user = getpwuid($row[1]);
+    if (! $user) {$user = "uid=$row[1]";}
+    print "creating report for user = $user, rank = $i, GB-years = $age_size\n";
+    system("./disk_report.pl -u $user $directory_label > ${output_dir}/${directory_label}_${user}.html");
+    $i++;
+}
+
+exit;
+#
+# make a query
+#
+sub make_query {    
+
+    my($dbh, $sth_ref) = @_;
+    $$sth_ref = $dbh->prepare($sql)
+        or die "Can't prepare $sql: $dbh->errstr\n";
+    
+    $rv = $$sth_ref->execute
+        or die "Can't execute the query $sql\n error: $sth->errstr\n";
+    
+    return 0;
+}
+sub round {
+    my ($float) = @_;
+    $rounded_float = sprintf("%.2f", $float);
+    return $rounded_float;
+}
+#
+# end of file
+#
