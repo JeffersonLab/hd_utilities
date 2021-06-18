e34a1c9f78ff01140fc32ea1b1e3e238ea5ce25d
Justin Stevens <jrsteven@jlab.org>
2021-06-18 07:51:21 -0400

    make pippim selector more useful for rho event study

---
+++ dirc/dselector/runSelector.C
@@ -0,0 +1,56 @@
+// macro to process analysis TTree with TSelector
+#include <iostream> 
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TSystem.h"
+   
+void runSelector(TString runNumber = "72422", TString myPath = "/cache/halld/RunPeriod-2019-11/analysis/ver01/tree_pippim__B4/merged/") 
+{
+  // Load DSelector library
+  gROOT->ProcessLine(".x $(ROOT_ANALYSIS_HOME)/scripts/Load_DSelector.C");
+  int Proof_Nthreads = 8;
+
+  // process signal 
+  TString sampleDir = myPath;
+  cout<<"running selector on files in: "<<sampleDir.Data()<<endl;
+  
+  TChain *chain = new TChain("pippim__B4_Tree");
+  TSystemDirectory dir(sampleDir, sampleDir);
+  TList *files = dir.GetListOfFiles();
+  int ifile = 0;
+  if(files) {
+	  TSystemFile *file;
+	  TString fileName;
+	  TIter next(files);
+	  
+	  // loop over files
+	  while ((file=(TSystemFile*)next())) {
+		  fileName = file->GetName();
+		  if(fileName.Contains(runNumber)) {
+			  cout<<fileName.Data()<<endl;
+			  
+			  // check if file corrupted
+			  TFile f(sampleDir+fileName);
+			  if(f.TestBit(TFile::kRecovered)) {
+				  cout<<"file corrupted -> skipping"<<endl;
+				  continue;
+			  }
+			  if(f.IsZombie()) {
+				  cout<<"file is a Zombie -> skipping"<<endl;
+				  continue;
+			  }
+			  
+			  // add file to chain
+			  chain->Add(sampleDir+fileName);
+			  ifile++;
+		  }
+	  }
+
+	  cout<<"total entries in TChain = "<<chain->GetEntries()<<" from "<<ifile<<" files"<<endl;
+	  DPROOFLiteManager::Process_Chain(chain, "DSelector_pippim.C+", Proof_Nthreads, Form("hist_pippim_%s.acc.root", runNumber.Data()));
+  }
+
+  return;
+}
