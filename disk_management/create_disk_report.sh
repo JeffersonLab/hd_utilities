bd4e7651aa1b0162ae62e43b24c2bd70d1565c37
Mark Ito <markito3@gmail.com>
2021-07-09 08:43:38 -0400

    Change top-level report-making script to drive new behavior of disk_report.pl.

--- disk_management/create_disk_report.sh
+++ disk_management/create_disk_report.sh
@@ -7,7 +7,7 @@ webdir=/group/halld/www/halldweb/html/disk_management
 dmdir=/group/halld/Software/hd_utilities/disk_management
 $dmdir/disk_database.pl $dir $token
 mkdir -pv $temp_output_dir
-$dmdir/disk_report.pl $token > $temp_output_dir/${token}_report.html
-$dmdir/disk_report_users.pl $token $temp_output_dir
+cd $temp_output_dir
+$dmdir/disk_report.pl -n 20 -m 50 $token
 
 cp -pv $temp_output_dir/*.html $webdir/
