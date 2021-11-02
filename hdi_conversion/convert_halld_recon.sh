6de2dd64cf15bda76ee528ab4ff27b2ac5265fa1
Mark Ito <marki@jlab.org>
2021-11-02 14:24:21 -0400

    converts the source files

--- hdi_conversion/convert_halld_recon.sh
+++ hdi_conversion/convert_halld_recon.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 hd_interface=/home/marki/git/hd_interface
-rm -rf halld_recon cc_to_move.txt h_to_move.txt
+hdi_conversion=/home/marki/git/hd_utilities/hdi_conversion
+rm -rf halld_recon h_to_move.txt
 #git clone git@github.com:jeffersonlab/halld_recon
 cp -pr halld_recon.save halld_recon # instead of clone for testing to save time
 cd halld_recon
 echo git filter-repo \\ > ../command.sh
-awk '{print "    --path "$1" \\"}' < $hd_interface/conversion/filter.txt >> ../command.sh
+awk '{print "    --path "$1" \\"}' < $hdi_conversion/filter.txt >> ../command.sh
 echo >> ../command.sh
 . ../command.sh
-git branch| awk '{print "git branch -D "$1}' | bash
-mkdir old
-git mv src old
-find $hd_interface -type f -name \*.cc > ../cc_to_move.txt
+git branch| awk '{print "git branch -D "$1}' | bash | grep -v "Deleted branch"
+git mv src old_src
+git commit -m "src moved to old_src" | grep -v "rename"
+find $hd_interface -type f -name \*.cc | $hdi_conversion/move_cc.sh
 find $hd_interface -type f -name \*.h > ../h_to_move.txt
-git add .
-git commit -m "move done"
-git rm -r old
-git add .
-git commit -m "old deleted"
+git commit -m "move done" | grep -v "rename"
+git rm -r old_src | grep -v "rm "
+git commit -m "old_src deleted" | grep -v "delete mode"
 git status
