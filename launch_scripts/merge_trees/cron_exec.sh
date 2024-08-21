01228fafb9d76b4b0f7690e8ccc15d9c5eff6de7
Alex Austregesilo <aaustreg@jlab.org>
2024-08-21 11:56:10 -0400

    proper environment

--- launch_scripts/merge_trees/cron_exec.sh
+++ launch_scripts/merge_trees/cron_exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date
-source ~/env_monitoring_launch.sh
+source ~/env_analysis_launch.sh
 export PATH=/site/bin:${PATH} #because .login isn't executed, and need this path for SWIF
 python ~/hd_utilities/launch_scripts/merge_trees/merge_trees.py ~/hd_utilities/launch_scripts/merge_trees/jobs_merge.config 0
 
