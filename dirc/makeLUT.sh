13cf5091b7db79d4722d99c9035de17e5ccba506
jrstevenjlab <jrsteven@jlab.org>
2025-05-26 10:54:34 -0400

    update makeLUT

--- dirc/makeLUT.sh
+++ dirc/makeLUT.sh
@@ -1,8 +1,13 @@
 
+# link macros
 ln -s $HD_UTILITIES_HOME/dirc/loadlib.C
 ln -s $HD_UTILITIES_HOME/dirc/glxlut_add.C
 ln -s $HD_UTILITIES_HOME/dirc/glxlut_avr.C
 ln -s $HD_UTILITIES_HOME/dirc/glxlut_convert.C
+
+# get includes and dictionaries (new for ROOT6)
+ln -s $HALLD_RECON_HOME/$BMS_OSNAME/include/lut_dirc/DrcLutNode.h
+ln -s $HALLD_RECON_HOME/src/.${BMS_OSNAME}/plugins/Analysis/lut_dirc/DrcLutNode_Dict_rdict.pcm
 
 #$HD_UTILITIES_HOME/dirc/loop_lut_dirc.py
 #$HD_UTILITIES_HOME/dirc/loop_lut_avr.py
