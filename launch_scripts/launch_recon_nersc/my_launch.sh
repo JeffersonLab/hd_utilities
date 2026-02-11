bf4982ea9779e1d257c985bd7c2ed60cb652af29
bgrube <bgrube@jlab.org>
2026-02-10 20:34:26 -0500

    Remove obsolete code.

--- launch_scripts/launch_recon_nersc/my_launch.sh
+++ launch_scripts/launch_recon_nersc/my_launch.sh
@@ -11,13 +11,8 @@ first_run_number=$5
 dividenb=$(echo "256 / $threadnb" | bc)
 halld_version_set="halld_recon-5.9.0"
 sed 's,BATCH,'$batch',g' script_nersc_test.temp > script_nersc_test.sh
-#sed 's,VERSION,'$version',g;s,THREADNB,'$threadnb',g' jana_recon_nersc.temp > jana_recon_nersc.config
 sed 's,THREADNB,'$threadnb',g' jana_recon_nersc.temp > jana_recon_nersc.config
-#sed 's,DIVIDENB,'$dividenb',g' script_nersc_multi_test.temp > script_nersc_multi_test.py
-#sed 's,DIVIDENB,'$dividenb',g' script_nersc_multi_test_temp.py > script_nersc_multi_test.py
 chmod +x script_nersc_test.sh
-# chmod +x script_nersc_multi_test.py
-#scp -r ../launch-$batch perlmutter-p1.nersc.gov:/global/cfs/cdirs/m3120/
 nersc_launch_dir=/global/cfs/cdirs/m3120/launch-$batch
 #nersc_launch_dir=$PWD
 workflow=recon_${run_period}_${version}_batchNERSC-multi
@@ -69,22 +64,14 @@ while [ $i -lt $number_of_nodes ]; do
     ((i++))  # Increment i
 done
 command3=" -sbatch -A m3120 --volume=\"$nersc_launch_dir:/launch-$batch\" --image=docker:jeffersonlab/gluex_almalinux_9:latest --module=cvmfs --time=5:00:00 -N $number_of_nodes --tasks-per-node=1 --cpus-per-task=256 --qos=regular -C cpu :: $nersc_launch_dir/script_nersc_multi_test.sh $nersc_launch_dir /launch-$batch/script_nersc_test.sh /launch-$batch/jana_recon_nersc.config halld_recon/$halld_version_set $dividenb"
-#command3=" -sbatch -A m3120 --volume=\"$nersc_launch_dir:/launch-$batch\" --image=docker:jeffersonlab/gluex_almalinux_9:latest --module=cvmfs --time=3:00:00 -N $number_of_nodes --tasks-per-node=1 --cpus-per-task=256 --qos=regular -C cpu :: $nersc_launch_dir/script_nersc_multi_test.sh $nersc_launch_dir $nersc_launch_dir/script_nersc_test.sh $nersc_launch_dir/jana_recon_nersc.config halld_recon/halld_recon-5.1.0"
 command4=$command1$command2$command3
 echo $command4 > exec_$run_number.sh
 #echo "$nersc_launch_dir/script_nersc_multi.sh $nersc_launch_dir $nersc_launch_dir/script_nerscc.sh $nersc_launch_dir/jana_recon_nersc.config halld_recon/halld_recon-5.0.0" > exec.sh
 chmod +x exec_$run_number.sh
-##scp ../launch-$batch perlmutter-p1.nersc.gov:$nersc_launch_dir/
 if [ $run_number == "$first_run_number" ]; then
     echo "in if"
     scp -r ../launch-$batch perlmutter-p1.nersc.gov:/global/cfs/cdirs/m3120/
 fi
 ./exec_$run_number.sh
 
-# run all jobs
-#CMD = ['srun', '-n', SLURM_JOB_NUM_NODES, LAUNCHDIR+'/run_shifter_multi.sh']
-#CMD += [workdir]          # arg 1:  top-level directory for job
-#CMD += [SCRIPTFILE]       # arg 2:  script to run inside shifter (all subsequent args are eventually passed to this script)
-#CMD += [CONFIG]           # arg 3:  JANA config file
-#CMD += [RECONVERSION]     # arg 4:  sim-recon version
 #srun -n $SLURM_JOB_NUM_NODES /path/to/launchdir/run_shifter_multi.sh test script.sh c.cfg halld0404
