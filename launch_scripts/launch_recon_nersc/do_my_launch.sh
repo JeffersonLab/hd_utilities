54c839bfaa1c1acc0d0ed702d6ec1dac1df0b6b5
bgrube <bgrube@jlab.org>
2026-02-13 20:09:54 -0500

    Use array to construct `swif2 -add-job` command line. Add TODOs. Improve comments.

--- launch_scripts/launch_recon_nersc/do_my_launch.sh
+++ launch_scripts/launch_recon_nersc/do_my_launch.sh
@@ -4,8 +4,8 @@
 # using swif2. One job is submitted for each run number in the given
 # list file.
 
-RUN_PERIOD="2025-01"
-VER="03"  # Version of this reconstruction launch
+RUN_PERIOD="2025-01"  # Run period to process.
+VER="03"  # Version of this reconstruction launch.
 RECON_VERSION="halld_recon/halld_recon-5.9.0"  # Version of reconstruction code to be used. The software environment is constructed from a CVMFS directory that mirrors the directory /group/halld/Software/builds/Linux_CentOS7-x86_64-gcc4.8.5-cntr. The given value should be a directory relative to that.  #TODO fix dir
 JANA_CONFIG="jana_recon_nersc.config"  # JANA config file to use; must be located in ${NERSC_LAUNCH_DIR}
 BATCH="${VER}-perl"  # Batch label used to hold details of the launch; is appended to workflow name and directory names; should make it clear where/how the campaign was being run.
@@ -26,6 +26,8 @@ NERSC_NMB_JOBS_PER_NODE=$(echo "${NERSC_NMB_TREADS_PER_NODE} / ${NERSC_NMB_TREAD
 NERSC_HOST="perlmutter-p1.nersc.gov"  # NERSC hostname to use for ssh.
 NERSC_IMAGE="docker:jeffersonlab/gluex_almalinux_9:latest"  # Shifter image that was converted from Docker image. Is not pulled in automatically and needs to exist in Shifter registry.
 
+# load run list into array
+#TODO improve code
 RUN_NUMBERS=( $( cat list-2025-01-ver03-perl.txt ) )
 
 
@@ -67,9 +69,14 @@ do
   #NERSC_NMB_NODES=$(echo "${NMB_EVIO_FILES} / ${NERSC_NMB_JOBS_PER_NODE}" | bc)
   NERSC_NMB_NODES=$(echo "(${NMB_EVIO_FILES} + ${NERSC_NMB_JOBS_PER_NODE} - 1) / ${NERSC_NMB_JOBS_PER_NODE}" | bc)
   echo "Number of nodes asked: ${NERSC_NMB_NODES}"
+  # construct command to submit swif2 job
+  # one swif2 job per NERSC node, each processing ${NERSC_NMB_JOBS_PER_NODE} evio files
   SWIF_JOB_NAME="GLUEX_recon_${RUN_NUMBER}"
-  command1="swif2 add-job -workflow ${SWIF_WORKFLOW} -name ${SWIF_JOB_NAME} "
-  command2=""
+  CMD=(
+    swif2 add-job
+    -workflow "${SWIF_WORKFLOW}"
+    -name "${SWIF_JOB_NAME}"
+  )
   for (( NERSC_NODE_INDEX=0; NERSC_NODE_INDEX < NERSC_NMB_NODES; NERSC_NODE_INDEX++ ))
   do
     EVIO_FILE_START_INDEX=$((NERSC_NODE_INDEX * NERSC_NMB_JOBS_PER_NODE))
@@ -84,16 +91,40 @@ do
     node_volatile="${SWIF_OUTPUT_ROOT}/${RUNDIR}"
     echo "mkdir -p ${node_volatile}"
     mkdir -p ${node_volatile}
-    #####command2+=" -name ${SWIF_JOB_NAME} -input ${node_cache} -output match:${RUNDIR}/* ${node_volatile}"
-    command2+=" ${node_cache} -output match:${RUNDIR}/* ${node_volatile}"
-    #echo "swif2 add-job -workflow ${SWIF_WORKFLOW} -name ${SWIF_JOB_NAME} -input ${node_cache} -output match:RUN${RUN_NUMBER}/FILE${i}/* ${node_volatile}"
+    CMD+=(
+      ${node_cache}
+      -output match:${RUNDIR}/* "${node_volatile}"
+    )
   done
-  command3=" -sbatch -A ${NERSC_PROJECT} --volume=\"${NERSC_LAUNCH_DIR}:/launch-${BATCH}\" --image=${NERSC_IMAGE} --module=cvmfs --time=5:00:00 -N ${NERSC_NMB_NODES} --tasks-per-node=1 --cpus-per-task=256 --qos=${NERSC_QOS} -C cpu :: ${NERSC_LAUNCH_DIR}/script_nersc_multi_test.sh ${NERSC_LAUNCH_DIR} /launch-${BATCH}/script_nersc_test.sh /launch-${BATCH}/${JANA_CONFIG} ${RECON_VERSION} ${NERSC_NMB_JOBS_PER_NODE}"
-  command4=$command1$command2$command3
-  echo ${command4} >| exec_${RUN_NUMBER}.sh
-  #echo "${NERSC_LAUNCH_DIR}/script_nersc_multi.sh ${NERSC_LAUNCH_DIR} ${NERSC_LAUNCH_DIR}/script_nerscc.sh ${NERSC_LAUNCH_DIR}/${JANA_CONFIG} halld_recon/halld_recon-5.0.0" > exec.sh
-  chmod +x exec_${RUN_NUMBER}.sh
-  ./exec_${RUN_NUMBER}.sh
+  CMD+=(
+    -sbatch
+      # these options are passed to `sbatch` when swif2 submits job at NERSC
+      -A "${NERSC_PROJECT}"
+      --volume=\""${NERSC_LAUNCH_DIR}:/launch-${BATCH}"\"
+      --image="${NERSC_IMAGE}"
+      --module=cvmfs
+      --time=5:00:00  #TODO replace with variable
+      -N ${NERSC_NMB_NODES}
+      --tasks-per-node=1
+      --cpus-per-task=256  #TODO replace with variable
+      --qos="${NERSC_QOS}"
+      -C cpu  #TODO replace with variable
+      ::
+      # job script to run at NERSC
+      "${NERSC_LAUNCH_DIR}/script_nersc_multi_test.sh"  # wrapper script for script_nersc_multi_test.py
+      # arguments passed to script_nersc_multi_test.py
+      "${NERSC_LAUNCH_DIR}"                    # LAUNCHDIR argument
+      "/launch-${BATCH}/script_nersc_test.sh"  # SCRIPTFILE argument
+      "/launch-${BATCH}/${JANA_CONFIG}"        # CONFIG argument
+      "${RECON_VERSION}"                       # RECONVERSION argument
+      "${NERSC_NMB_JOBS_PER_NODE}"             # SLURM_JOBS_PER_NODE
+  )
+  # generate shell-escaped version of command array and write it to file so it becomes a script that can be run directly
+  echo "${CMD[@]}" >| "./exec_${RUN_NUMBER}.sh"
+  # {
+  #   printf '%q ' "${CMD[@]}"  #TODO this would be the safer approach, but in `-output match:RUN132194/FILE024/*` this would escape the `*`; not sure if this would cause problems with swif2
+  #   printf '\n'
+  # } >| "exec_${RUN_NUMBER}.sh"
+  chmod +x "./exec_${RUN_NUMBER}.sh"
+  "./exec_${RUN_NUMBER}.sh"
 done
-
-#srun -n $SLURM_JOB_NUM_NODES /path/to/launchdir/run_shifter_multi.sh test script.sh c.cfg halld0404
