fae115543906533867875ca57a851b04abbfdaaa
bgrube <bgrube@jlab.org>
2026-02-17 17:31:33 -0500

    Use `__main__` guard and argparse. Improve naming. Add TODOs. Remove obsolete code.

--- launch_scripts/launch_recon_nersc/script_nersc_multi_test.py
+++ launch_scripts/launch_recon_nersc/script_nersc_multi_test.py
@@ -1,103 +1,112 @@
 #!/usr/bin/env python3
 
-# This script is called from the script-multi.sh script
-# which just serves as a wrapper for this one. It wakes
-# up in the top-level working directory for the job.
-# It sets up the directories for each of the sub-jobs
-# and then calls srun to launch those jobs on all of
-# the remote nodes.
+"""
+This job script is called from the script-multi.sh script which just
+serves as a wrapper for this one. It wakes up in the top-level working
+directory for the job, sets up the directories for each of the
+slurm task, and then calls `srun` to launch the tasks on all nodes.
+"""
 
-import os
-import sys
+import argparse
 import glob
-import subprocess
 import math
-
-# This is the directory where the job scripts will be held.
-# For the actual jobs, it will be the "launch" directory
-# which has been checked out from the halld subversion
-# repository. Here, we just point to the testing directory.
-#TODO use argparse to make this more robust and self-documenting
-LAUNCH_DIR             = sys.argv[1]
-SCRIPT_FILE            = sys.argv[2]
-JANA_CONFIG            = sys.argv[3]
-HALLD_VERSION_SET_XML  = sys.argv[4]
-NMB_PROCESSES_PER_NODE = float(sys.argv[5])
-NMB_TREADS_PER_PROCESS = float(sys.argv[6])
-
-workdir = os.getcwd()
-
-SLURM_JOB_NUM_NODES     = os.getenv("SLURM_JOB_NUM_NODES")  # number of nodes allocated for this job
-# SLURM_JOB_CPUS_PER_NODE = os.getenv("SLURM_JOB_CPUS_PER_NODE")  # number of CPUs available to the job on the allocated nodes
-
-# Get list of raw data files
-eviofiles = sorted(glob.glob("hd_rawdata_*.evio"))
-
-# Calculate the expected number of nodes
-expected_nodes = math.ceil(float(len(eviofiles)) / NMB_PROCESSES_PER_NODE) #Integer division
-
-print(f"LAUNCH_DIR: {LAUNCH_DIR}")
-print(f"SCRIPT_FILE: {SCRIPT_FILE}")
-print(f"JANA_CONFIG: {JANA_CONFIG}")
-print(f"HALLD_VERSION_SET_XML: {HALLD_VERSION_SET_XML}")
-print(f"Nb of evio files: {len(eviofiles)}")
-print(f"Nb of nodes: {expected_nodes}")
-print(f"Nb of nodes asked: {SLURM_JOB_NUM_NODES}")
-
-# Verify that the expected number of nodes matches the SLURM_JOB_NUM_NODES
-if int(expected_nodes) != int(SLURM_JOB_NUM_NODES):
-  print(f"MISMATCH IN NUMBER OF EVIO FILES PER NODE! #EVIO={len(eviofiles)}  "
-        f"Expected Nodes={expected_nodes}  SLURM_JOB_NUM_NODES={SLURM_JOB_NUM_NODES}")
-  sys.exit(101)
-
-  # # Verify that we have exactly one node per raw data file
-  # if len(expected_nodes) != int(SLURM_JOB_NUM_NODES):
-  #   print(f"MISMATCH IN NUMBER OF EVIO FILES AND SLURM NODES! #EVIO={len(eviofiles)}  SLURM_JOB_NUM_NODES={int(SLURM_JOB_NUM_NODES)}")
-  #   sys.exit(101)
-
-# Loop over raw data files
-for i, eviofile in enumerate(eviofiles):
-  # Get RUN/FILE numbers from file names
-  run = int(eviofile[11:17])
-  fil = int(eviofile[18:21])
-
-  # Make subjob directory
-  #TODO why is this called for every file? we know how many files each job processes
-  RUNDIR = f"RUN{int(run):06d}/FILE{int(float(fil) / NMB_PROCESSES_PER_NODE):03d}"
-  os.makedirs(RUNDIR, exist_ok = True)
-
-  # Make symlink pointing to subjobdir so the subjob
-  # can cd into it via SLURM_NODEID
-  #TODO why is this extra step needed? why not use FILE%03d directly in the subjob script? The only difference is the number of digits used
-  subjobdir = f"subjob{int(float(fil) / NMB_PROCESSES_PER_NODE):04d}"
-  if not os.path.exists(subjobdir):
-    os.symlink(RUNDIR, subjobdir)
-
-  # Make symlink in subjobdir to evio file
-  os.symlink(f"../../{eviofile}", f"{RUNDIR}/{eviofile}")
+import os
+import subprocess
+import sys
 
 
-# run one task per node
-# each task will run ${NMB_PROCESSES_PER_NODE} hd_root processes in parallel, each processing a different evio file)
-CMD = [
-  "srun",
-  f"--ntasks={SLURM_JOB_NUM_NODES}",  # 1 task per node
-  "--output=task-%x-%j-%t.out",  # write stdout and stderr of task to file named task-<job name>-<job id>-<task id>.out in working directory of task
-  f"{LAUNCH_DIR}/run_shifter_multi.sh",  # script to run as task
-  workdir,                 # arg 1:  top-level directory for job
-  SCRIPT_FILE,             # arg 2:  script to run inside shifter (all subsequent args are eventually passed to this script)
-  JANA_CONFIG,             # arg 3:  JANA config file
-  HALLD_VERSION_SET_XML,   # arg 4:  Hall-D version set XML file
-  NMB_TREADS_PER_PROCESS,  # arg 5:  Number of threads of `hd_root` process
-]
-# n.b. run/file are derived from evio file names. (see run_shifter_multi.sh)
-print(f"Nb of nodes asked: {CMD}")
-print(" ".join(CMD))
-with subprocess.Popen(CMD, stdout = subprocess.PIPE, stderr = subprocess.PIPE) as proc:
-  outs, errs = proc.communicate()
-  with open("std.out", "wb") as f:
-    f.write(outs)
-  with open("std.err", "wb") as f:
-    f.write(errs)
-  with open("exitcode", "w") as f:
-    f.write(f"{proc.returncode:d}")
+def main(args: argparse.Namespace) -> None:
+
+  work_dir_job = os.getcwd()  # directory where the job scripts are held; for the actual jobs, it is the "launch" directory  #TODO is this really true? doesn't swif2 define the working directory for the job?
+
+  SLURM_JOB_NUM_NODES = os.getenv("SLURM_JOB_NUM_NODES")  # number of nodes allocated for this job
+  evio_file_names = sorted(glob.glob("hd_rawdata_*.evio"))  # list of raw data file names
+  # calculate the expected number of nodes
+  expected_nmb_nodes = math.ceil(float(len(evio_file_names)) / args.nmb_processes_per_node)  # integer division
+
+  #TODO use a more elegant way to print the arguments
+  print(f"LAUNCH_DIR: {args.launch_dir}")
+  print(f"SCRIPT_FILE: {args.task_script_file}")
+  print(f"JANA_CONFIG: {args.jana_config}")
+  print(f"HALLD_VERSION_SET_XML: {args.halld_version_set_xml}")
+  print(f"NMB_PROCESSES_PER_NODE: {args.nmb_processes_per_node}")
+  print(f"NMB_THREADS_PER_PROCESS: {args.nmb_threads_per_process}")
+  print(f"Nb of evio files: {len(evio_file_names)}")
+  print(f"Nb of nodes: {expected_nmb_nodes}")
+  print(f"Nb of nodes asked: {SLURM_JOB_NUM_NODES}")
+
+  # verify that the expected number of nodes matches the SLURM_JOB_NUM_NODES
+  if int(expected_nmb_nodes) != int(SLURM_JOB_NUM_NODES):
+    print(f"MISMATCH IN NUMBER OF EVIO FILES PER NODE! #EVIO={len(evio_file_names)}  "
+      f"Expected Nodes={expected_nmb_nodes}  SLURM_JOB_NUM_NODES={SLURM_JOB_NUM_NODES}")
+    sys.exit(101)
+
+  # Loop over raw data files
+  for evio_file_name in evio_file_names:
+    # get RUN/FILE numbers from file names
+    run  = int(evio_file_name[11:17])
+    file = int(evio_file_name[18:21])
+
+    # make subjob directory
+    #TODO why is this called for every file? we know how many files each job processes
+    run_dir_name = f"RUN{int(run):06d}/FILE{int(float(file) / args.nmb_processes_per_node):03d}"
+    os.makedirs(run_dir_name, exist_ok = True)
+
+    # make symlink pointing to subjobdir so the subjob
+    # can cd into it via SLURM_NODEID
+    #TODO why is this extra step needed? why not use FILE%03d directly in the subjob script? The only difference is the number of digits used
+    subjob_dir_name = f"subjob{int(float(file) / args.nmb_processes_per_node):04d}"
+    if not os.path.exists(subjob_dir_name):
+      os.symlink(run_dir_name, subjob_dir_name)
+
+    # make symlink in subjob directory to evio file
+    os.symlink(f"../../{evio_file_name}", f"{run_dir_name}/{evio_file_name}")
+
+
+  # run one task per node
+  # each task will run args.nmb_processes_per_node hd_root processes in parallel, each processing a single evio file)
+  command = [
+    "srun",
+    f"--ntasks={SLURM_JOB_NUM_NODES}",  # 1 task per node
+    # "--output=task-%x-%j-%t.out",  # write stdout and stderr of task to file named task-<job name>-<job id>-<task id>.out in working directory of task
+    f"{args.launch_dir}/run_shifter_multi.sh",  # script to run as task
+    work_dir_job,                  # arg 1:  top-level directory of job
+    args.task_script_file,         # arg 2:  script to run inside shifter (all subsequent args are eventually passed to this script)
+    args.jana_config,              # arg 3:  JANA config file
+    args.halld_version_set_xml,    # arg 4:  Hall-D version set XML file
+    args.nmb_threads_per_process,  # arg 5:  number of threads of `hd_root` process
+  ]
+  # n.b. run/file are derived from evio file names. (see run_shifter_multi.sh)
+  print(f"Nb of nodes asked: {command}")
+  print(" ".join(command))
+  with subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE) as srun:
+    stdout, stderr = srun.communicate()
+    #TODO is this meaningful? wouldn't it be better to capture the output of each task separately using `srun --output`? the exit code also does not seem to be used
+    with open("./std.out", "wb") as f:
+      f.write(stdout)
+    with open("./std.err", "wb") as f:
+      f.write(stderr)
+    with open("./exitcode", "w") as f:
+      f.write(f"{srun.returncode:d}")
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+    description = "Prepare directory structure and use srun to start a reconstruction task on each node (positional args).",
+  )
+  parser.add_argument("launch_dir",              help = "Path to launch directory containing scripts and config files")
+  parser.add_argument("task_script_file",        help = "Script file to run as task on each node")
+  parser.add_argument("jana_config",             help = "JANA config file")
+  parser.add_argument("halld_version_set_xml",   help = "Hall-D version set XML file")
+  parser.add_argument("nmb_processes_per_node",  help = "Number of processes per node",            type = int)
+  parser.add_argument("nmb_threads_per_process", help = "Number of threads per `hd_root` process", type = int)
+  args = parser.parse_args()
+  main(args)
+
+  #TODO use named arguments
+  # parser.add_argument("--launch-dir",              dest = "launch_dir",              required = True, help = "Path to launch directory containing scripts and config files")
+  # parser.add_argument("--task-script-file",        dest = "task_script_file",        required = True, help = "Script file to run as task on each node")
+  # parser.add_argument("--jana-config",             dest = "jana_config",             required = True, help = "JANA config file")
+  # parser.add_argument("--halld-version-set-xml",   dest = "halld_version_set_xml",   required = True, help = "Hall-D version set XML file")
+  # parser.add_argument("--nmb-processes-per-node",  dest = "nmb_processes_per_node",  required = True, help = "Number of processes per node",            type = int)
+  # parser.add_argument("--nmb-threads-per-process", dest = "nmb_threads_per_process", required = True, help = "Number of threads per `hd_root` process", type = int)
