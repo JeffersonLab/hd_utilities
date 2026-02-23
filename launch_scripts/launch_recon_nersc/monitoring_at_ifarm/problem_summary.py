d384427054843bca1ad0dbe2b990018efb2f978b
bgrube <bgrube@jlab.org>
2026-02-23 18:29:08 -0500

    Add docstring. Make command-line calls more robust. Make parsing of swif2 output more robust. Improve console output. Use f-strings.

--- launch_scripts/launch_recon_nersc/monitoring_at_ifarm/problem_summary.py
+++ launch_scripts/launch_recon_nersc/monitoring_at_ifarm/problem_summary.py
@@ -1,67 +1,99 @@
 #!/usr/bin/env python3
-#
-# This script is a convenience for printing the count of each
-# problem type in a given workflow. Run like this:
-#
-#   ./problem_summary.py recon_2018-01_ver02_batch04
-#
-#
 
-import os
-import sys
+"""
+This is a convenience script for printing the count of each problem
+type for all jobs in a given swif2 workflow.
+"""
+
+from __future__ import annotations
+
+import argparse
+from collections import defaultdict
+import io
 import subprocess
 
-if len(sys.argv) < 2:
-    print('\nUsage:\n    problem_summary.py workflow\n\n')
-    print('workflow is the swif2 workflow. See a list with swif2 list\n')
-    sys.exit(0)
+import dotenv
 
-workflow = sys.argv[1]
 
-# Get list of problem jobs
-lines = subprocess.Popen(['swif2', 'status', '-workflow', workflow], stdout=subprocess.PIPE).communicate()[0]
-lines = lines.decode('utf-8')  # decode bytes to string for Python 3
-#print(lines)
-for line in lines.split('\n'):
-    toks = line.split()
-    if len(toks) < 3:
-        continue
-    key = toks[0]
-    vals = toks[2:]
-    if key == 'jobs':
-        Njobs = vals[0]
-    if key == 'dispatched':
-        Ndispatched = vals[0]
-    if key == 'succeeded':
-        Nsucceeded = vals[0]
-    if key == 'problems':
-        Nproblems = vals[0]
-    if key == 'problem_types':
-        problems = vals[0].split(',')
+def main(args: argparse.Namespace) -> None:
+  swif2_workflow_name = args.workflow
 
-# Find count of each problem type
-Nprobs = {}
-lines = subprocess.Popen(['swif2', 'status', '-workflow', workflow, '-problems'], stdout=subprocess.PIPE).communicate()[0]
-lines = lines.decode('utf-8')  # decode bytes to string for Python 3
-for line in lines.split('\n'):
-    for problem in problems:
-        if problem in line:
-            if problem not in Nprobs.keys():
-                Nprobs[problem] = 0
-            Nprobs[problem] += 1
+  # get job summary for the workflow
+  try:
+    result = subprocess.run(
+      [
+        "swif2",
+        "status",
+        "-workflow",
+        swif2_workflow_name,
+      ],
+      capture_output = True,
+      text = True,
+      check = True,
+    )
+  except subprocess.CalledProcessError as e:
+    print(f"'{e.cmd}' failed with error: '{e.stderr}'")
+    return
+  # parse `'key' = 'value'` pairs returned by `swif2 status` command into a dictionary
+  swif2_info = dotenv.dotenv_values(stream = io.StringIO(result.stdout))
 
-# Print the summary
-print('=======================================================')
-print('   workflow: ' + workflow)
-print('      Njobs: ' + str(Njobs))
-print('Ndispatched: ' + str(Ndispatched))
-print('  Nproblems: ' + str(Nproblems))
-for problem, cnt in Nprobs.items():
-    print('         %4d - %s' % (cnt, problem))
-print('=======================================================')
-print('')
-print('Retry command:\n')
+  # process command output to extract problem types and their counts
+  if "jobs" in swif2_info:
+    nmb_jobs = swif2_info["jobs"]
+  if "dispatched" in swif2_info:
+    nmb_jobs_dispatched = swif2_info["dispatched"]
+  if "succeeded" in swif2_info:
+    nmb_jobs_succeeded = swif2_info["succeeded"]
+  if "problems" in swif2_info:
+    nmb_jobs_problems = swif2_info["problems"]
+  if "problem_types" in swif2_info:
+    job_problems = swif2_info["problem_types"].split(",")
 
-cmd = ['swif2', 'retry-jobs', '-workflow', workflow, '-problems'] + problems
-print(' '.join(cmd))
-print('')
+  # get runs with problems
+  try:
+    result = subprocess.run(
+      [
+        "swif2",
+        "status",
+        "-workflow",
+        swif2_workflow_name,
+        "-problems",
+      ],
+      capture_output = True,
+      text = True,
+      check = True,
+    )
+  except subprocess.CalledProcessError as e:
+    print(f"'{e.cmd}' failed with error: '{e.stderr}'")
+    return
+
+  # process command output to count each problem type
+  #NOTE cannot easily apply dotenv here as keys are repeated for each job
+  nmb_problems: defaultdict[str, int] = defaultdict(int)  # problem type -> count
+  for line in result.stdout.splitlines():
+    for problem in job_problems:
+      if problem in line:
+        nmb_problems[problem] += 1
+
+  # print summary
+  print("=======================================================")
+  print(f"Summary of problems for swif2 workflow")
+  print("-------------------------------------------------------")
+  print(f" workflow name: {swif2_workflow_name}")
+  print(f"number of jobs: {nmb_jobs}")
+  print(f"    dispatched: {nmb_jobs_dispatched}")
+  print(f"     succeeded: {nmb_jobs_succeeded}")
+  print(f" with problems: {nmb_jobs_problems}")
+  print("-------------------------------------------------------")
+  print("Count by problem type:")
+  for problem, count in nmb_problems.items():
+    print(f"    {count:4d} - {problem}")
+  print("=======================================================\n")
+  print("Retry command:\n")
+  print(f"swif2 retry-jobs -workflow {swif2_workflow_name} -problems {' '.join(job_problems)}\n")
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Print the count of each problem type for all jobs in a given swif2 workflow.")
+  parser.add_argument("workflow", help="The swif2 workflow name. See a list by running 'swif2 list'")
+  main(parser.parse_args())
