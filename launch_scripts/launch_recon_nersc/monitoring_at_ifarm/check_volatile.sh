dfe3b2300c5b738ea31cedce91f7a211cc74fd2a
bgrube <bgrube@jlab.org>
2026-02-23 19:15:42 -0500

    Adapt to new directory layout for tasks. Improve variable naming. Make glob pattern for run number more specific. Indeed wite number if label says so.

--- launch_scripts/launch_recon_nersc/monitoring_at_ifarm/check_volatile.sh
+++ launch_scripts/launch_recon_nersc/monitoring_at_ifarm/check_volatile.sh
@@ -1,45 +1,47 @@
-rp="${1}"
-ve="${2}"
-nb="${3}"
-ba="${4}"
+RUN_PERIOD="${1}"
+BATCH="${2}"
+TASK_INDEX="${3}"
+VER="${4}"
 
-SWIF_WORKFLOW="recon_${rp}_${ve}_batchNERSC-multi"
-SWIF_OUTPUT_ROOT="/volatile/halld/offsite_prod/RunPeriod-${rp}/recon/${ve}"
+
+SWIF_WORKFLOW="recon_${RUN_PERIOD}_ver${BATCH}_NERSC-multi"
+SWIF_OUTPUT_ROOT="/lustre/expphy/volatile/halld/offsite_prod/RunPeriod-${RUN_PERIOD}/recon/ver${BATCH}"
+SUBDIR_TASK=$(printf "RUN??????/TASK%03d" "${TASK_INDEX}")
 
 swif2 status "${SWIF_WORKFLOW}"
 
 echo "Number of runs processed"
-ls -lrth "${SWIF_OUTPUT_ROOT}/RUN*/FILE${nb}/RUN*/FILE${nb}/exitcode_0.txt" | wc -l
+ls -1 "${SWIF_OUTPUT_ROOT}/${SUBDIR_TASK}/exitcode_0.txt" | wc -l
 
 echo "Number of runs not moved yet"
-ls -lrth "${SWIF_OUTPUT_ROOT}/RUN*/FILE${nb}/RUN*/FILE${nb}/dana*_000.hddm"
+ls -1 "${SWIF_OUTPUT_ROOT}/${SUBDIR_TASK}/dana*_000.hddm" | wc -l
 
 echo "Number of rawdata files coming back"
-ls -lrth "${SWIF_OUTPUT_ROOT}/RUN*/FILE${nb}/RUN*/FILE${nb}/hd_rawdata_??????_???.evio"
-rm "${SWIF_OUTPUT_ROOT}/RUN??????/FILE???/RUN??????/FILE???/hd_rawdata_??????_???.evio"
+ls -1 "${SWIF_OUTPUT_ROOT}/${SUBDIR_TASK}/hd_rawdata_??????_???.evio" | wc -l
+rm "${SWIF_OUTPUT_ROOT}/RUN??????/TASK???/hd_rawdata_??????_???.evio"
 
 swif2 status "${SWIF_WORKFLOW}"
 
 echo "Number of runs cooked:"
-ls -lrth "${SWIF_OUTPUT_ROOT}/RUN*/FILE${nb}/RUN*/FILE${nb}/dana*_000.hddm" | wc -l
+ls -1 "${SWIF_OUTPUT_ROOT}/${SUBDIR_TASK}/dana*_000.hddm" | wc -l
 
 echo "Number of runs cooked with .tgz:"
-ls -lrth "${SWIF_OUTPUT_ROOT}/RUN*/FILE${nb}/RUN*/FILE${nb}/*_000.tgz" | wc -l
+ls -1 "${SWIF_OUTPUT_ROOT}/${SUBDIR_TASK}/*_000.tgz" | wc -l
 
 echo "Number of files cooked with and without .tgz:"
-ls -lrth "${SWIF_OUTPUT_ROOT}/RUN*/FILE*/RUN*/FILE*/dana*.hddm" | wc -l
+ls -1 "${SWIF_OUTPUT_ROOT}/${SUBDIR_TASK}/dana*.hddm" | wc -l
 
 echo "Number of runs cooked and cached:"
-ls "/cache/halld/RunPeriod-${rp}/recon/${ba}/REST/*/*_000.hddm" | wc -l
+ls -1 "/cache/halld/RunPeriod-${RUN_PERIOD}/recon/ver${VER}/REST/*/*_000.hddm" | wc -l
 
 echo "Number of runs cooked and on tape:"
-ls "/mss/halld/RunPeriod-${rp}/recon/${ba}/REST/*/*_000.hddm" | wc -l
+ls -1 "/mss/halld/RunPeriod-${RUN_PERIOD}/recon/ver${VER}/REST/*/*_000.hddm" | wc -l
 
 echo "Number of runs checked and ordered not moved yet:"
-ls -lrth "${SWIF_OUTPUT_ROOT}/${ba}/REST/*/dana*_000.hddm"
+ls -1 "${SWIF_OUTPUT_ROOT}/ver${VER}/REST/*/dana*_000.hddm" | wc -l
 
 echo "Number of runs checked and ordered"
-ls "${SWIF_OUTPUT_ROOT}/${ba}/REST/" | wc -l
+ls -1 "${SWIF_OUTPUT_ROOT}/ver${VER}/REST/" | wc -l
 
 echo "Number of REST files checked and ordered:"
-ls "${SWIF_OUTPUT_ROOT}/${ba}/REST/*/dana*_???.hddm" | wc -l
+ls -1 "${SWIF_OUTPUT_ROOT}/ver${VER}/REST/*/dana*_???.hddm" | wc -l
