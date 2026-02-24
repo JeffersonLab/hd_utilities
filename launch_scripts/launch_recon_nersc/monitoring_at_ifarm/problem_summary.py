809dea7fe3f1dc491d25a0605596b185153afe08
bgrube <bgrube@jlab.org>
2026-02-23 19:29:16 -0500

    Make parsing code more robust. Treat case where workflow has no errors.

--- launch_scripts/launch_recon_nersc/monitoring_at_ifarm/problem_summary.py
+++ launch_scripts/launch_recon_nersc/monitoring_at_ifarm/problem_summary.py
@@ -15,6 +15,19 @@ import subprocess
 import dotenv
 
 
+def ensure_value_exists(
+  config: dict[str, str | None],
+  key:    str,
+) -> str:
+  """Ensure that the given key exists in the config dictionary and has a non-None value; return the value."""
+  if key not in config:
+    raise KeyError(f"Missing '{key}' key in .env file")
+  value = config[key]
+  if value is None:
+    raise ValueError(f"Missing value for '{key}' key in .env file")
+  return value
+
+
 def main(args: argparse.Namespace) -> None:
   swif2_workflow_name = args.workflow
 
@@ -38,16 +51,13 @@ def main(args: argparse.Namespace) -> None:
   swif2_info = dotenv.dotenv_values(stream = io.StringIO(result.stdout))
 
   # process command output to extract problem types and their counts
-  if "jobs" in swif2_info:
-    nmb_jobs = swif2_info["jobs"]
-  if "dispatched" in swif2_info:
-    nmb_jobs_dispatched = swif2_info["dispatched"]
-  if "succeeded" in swif2_info:
-    nmb_jobs_succeeded = swif2_info["succeeded"]
-  if "problems" in swif2_info:
-    nmb_jobs_problems = swif2_info["problems"]
-  if "problem_types" in swif2_info:
-    job_problems = swif2_info["problem_types"].split(",")
+  nmb_jobs            = int(ensure_value_exists(swif2_info, "jobs"))
+  nmb_jobs_dispatched = int(ensure_value_exists(swif2_info, "dispatched"))
+  nmb_jobs_succeeded  = int(ensure_value_exists(swif2_info, "succeeded"))
+  nmb_jobs_problems   = int(ensure_value_exists(swif2_info, "problems"))
+  job_problems: list[str] = []
+  if nmb_jobs_problems > 0:
+    job_problems = ensure_value_exists(swif2_info, "problem_types").split(",")
 
   # get runs with problems
   try:
@@ -84,13 +94,18 @@ def main(args: argparse.Namespace) -> None:
   print(f"    dispatched: {nmb_jobs_dispatched}")
   print(f"     succeeded: {nmb_jobs_succeeded}")
   print(f" with problems: {nmb_jobs_problems}")
-  print("-------------------------------------------------------")
-  print("Count by problem type:")
-  for problem, count in nmb_problems.items():
-    print(f"    {count:4d} - {problem}")
-  print("=======================================================\n")
-  print("Retry command:\n")
-  print(f"swif2 retry-jobs -workflow {swif2_workflow_name} -problems {' '.join(job_problems)}\n")
+  if nmb_jobs_problems > 0:
+    print(f"  problem types: {', '.join(job_problems)}")
+    print("-------------------------------------------------------")
+    print("Count by problem type:")
+    for problem, count in nmb_problems.items():
+      print(f"    {count:4d} - {problem}")
+    print("=======================================================\n")
+    print("Retry command:\n")
+    print(f"swif2 retry-jobs -workflow {swif2_workflow_name} -problems {' '.join(job_problems)}\n")
+  else:
+    print("=======================================================\n")
+    print("No problems found. No retry needed.\n")
 
 
 if __name__ == "__main__":
