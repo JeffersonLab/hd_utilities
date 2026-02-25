e31b7d0c017f1b1cbd397a74fd6ed8ffcef512a1
bgrube <bgrube@jlab.org>
2026-02-24 20:14:27 -0500

    Remove obsolete scripts.

--- launch_scripts/launch_recon_nersc/monitoring_at_nersc/check_core2.sh
+++
@@ -1,2 +0,0 @@
-ls -lrth running_dir2/jobs/gxproj4/*_recon_*/*/RUN*/FILE*/core*
-chmod og+rw running_dir2/jobs/gxproj4/*_recon_*/*/RUN*/FILE*/core*
--- launch_scripts/launch_recon_nersc/monitoring_at_nersc/check_dana2.sh
+++
@@ -1,2 +0,0 @@
-src=$1
-ls -lrth running_dir2/jobs/gxproj4/$src/*/RUN0*/FILE*/dana*
