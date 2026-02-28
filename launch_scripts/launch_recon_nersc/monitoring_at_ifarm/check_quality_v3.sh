472f99ab67e5016ebb79ba656195855dc62d1931
bgrube <bgrube@jlab.org>
2026-02-27 19:40:30 -0500

    Use quotes and braces. Improve naming. Add TODOs.

--- launch_scripts/launch_recon_nersc/monitoring_at_ifarm/check_quality_v3.sh
+++ launch_scripts/launch_recon_nersc/monitoring_at_ifarm/check_quality_v3.sh
@@ -1,34 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-rp=$1
-ve=$2
-pe=$3
+# Checks if expected output files exist for each raw data file.
+# Writes list of good and bad files into separate text files.
 
-volatile_recon_dir=/volatile/halld/offsite_prod/RunPeriod-$rp/recon/$pe/$ve/REST
+#TODO also check whether ROOT files are actually readable?
 
-volatile_hists=/volatile/halld/offsite_prod/RunPeriod-$rp/recon/$pe/$ve/hists
 
-volatile_flux_dir=/volatile/halld/offsite_prod/RunPeriod-$rp/recon/$pe/$ve/tree_PSFlux
-##mkdir -p $volatile_hists
+RUN_PERIOD="${1}"  # run period, e.g. "2017-01"
+VER="${2}"  # reconstruction version, e.g. "ver01"
+PROD_ENV="${3}"  # production environment, e.g. "ver01-perl"
 
-ls -1 $volatile_recon_dir > rp-$rp-files-to-copy-to-cache.txt
+VOLATILE_ROOT_DIR="/volatile/halld/offsite_prod/RunPeriod-${RUN_PERIOD}/recon/${PROD_ENV}/${VER}"
+VOLATILE_REST_DIR="${VOLATILE_ROOT_DIR}/REST"
+VOLATILE_HISTS_DIR="${VOLATILE_ROOT_DIR}/hists"
+# mkdir -p "${VOLATILE_HISTS_DIR}"
+VOLATILE_FLUX_DIR="${VOLATILE_ROOT_DIR}/tree_PSFlux"
 
-mss_dir=/mss/halld/RunPeriod-$rp/rawdata
+ls -1 "${VOLATILE_REST_DIR}" > "rp-${RUN_PERIOD}-files-to-copy-to-cache.txt"
 
-mss_recon_dir=/mss/halld/RunPeriod-$rp/recon/$ve
+RAW_DATA_DIR="/mss/halld/RunPeriod-${RUN_PERIOD}/rawdata"
+RECON_DIR_MSS="/mss/halld/RunPeriod-${RUN_PERIOD}/recon/${VER}"
+RECON_DIR_CACHE="/cache/halld/RunPeriod-${RUN_PERIOD}/recon/${VER}"
+WORK_DIR="/work/halld/home/gxproj4/offsite_prod/RunPeriod-${RUN_PERIOD}"
+VOLATILE_DIR="/volatile/halld/recon/RunPeriod-${RUN_PERIOD}/${VER}"
 
-cache_recon_dir=/cache/halld/RunPeriod-$rp/recon/$ve
+RUN_NUMBERS=($(cat "rp-${RUN_PERIOD}-files-to-copy-to-cache.txt"))
 
-work_dir=/work/halld/home/gxproj4/offsite_prod/RunPeriod-$rp
-
-volatile_dir=/volatile/halld/recon/RunPeriod-$rp/$ve
-
-rnb_tab=( $( cat rp-$rp-files-to-copy-to-cache.txt ) )
-
-rm good-list-$rp.txt
-touch good-list-$rp.txt
-rm bad-list-$rp.txt
-touch bad-list-$rp.txt
+rm "good-list-${RUN_PERIOD}.txt"
+touch "good-list-${RUN_PERIOD}.txt"
+rm "bad-list-${RUN_PERIOD}.txt"
+touch "bad-list-${RUN_PERIOD}.txt"
 
 i1=0
 i2=0
@@ -37,67 +38,81 @@ i4=0
 i5=0
 i6=0
 i7=0
-for rnb in "${rnb_tab[@]}"; do
-    echo "rnb: $rnb"
-    file_tab=($(ls $mss_dir/Run$rnb/hd_rawdata_??????_???.evio))
-    i7=0
-    ##mkdir -p $volatile_hists/$rnb
-    for file in "${file_tab[@]}"; do
-	read rnb < <(echo "$file" | grep -o '[0-9]\+' | tail -n 2)
-        read enb < <(echo "$file" | grep -o '[0-9]\+' | tail -n 1)
-	#echo "rnb: $rnb - enb: $enb"
-	#echo "file: $file"
-	file1a=$cache_recon_dir/REST/$rnb/dana_rest_${rnb}_${enb}.hddm
-	file2a=$cache_recon_dir/tree_PSFlux/$rnb/tree_PSFlux_${rnb}_${enb}.root
-	file3a=$cache_recon_dir/hists/${rnb}/hd_root_${rnb}_${enb}.root
-	file1b=$mss_recon_dir/REST/$rnb/dana_rest_${rnb}_${enb}.hddm
-	file2b=$mss_recon_dir/tree_PSFlux/$rnb/tree_PSFlux_${rnb}_${enb}.root
-	file3b=$mss_recon_dir/hists/${rnb}/hd_root_${rnb}_${enb}.root
-	#filehe=$work_dir/job_info_${rnb}_$enb/helicity.log
-	filear=$volatile_dir/REST/$rnb/dana_rest_${rnb}_${enb}.hddm
-	fileaf=$volatile_dir/tree_PSFlux/$rnb/tree_PSFlux_${rnb}_${enb}.root
-	fileah=$volatile_dir/hists/${rnb}/hd_root_${rnb}_${enb}.root
-	if { [[ -f $file1a ]] || [[ -f $file1b ]]; } && \
-	   { [[ -f $file2a ]] || [[ -f $file2b ]]; } && \
-	   { [[ -f $file3a ]] || [[ -f $file3b ]]; }; then
-	    #echo "$rnb $enb" >> good-list.txt
-	    ((i1++))
-	else
-	    echo "$rnb $enb" >> bad-list-$rp.txt
-	    #echo "Missing outputs $rnb $enb"
-	    ((i2++))
-	    ((i7++))
-	fi
-	if [[ -e $file1a || -e $file1b ]]; then
-	    #if [ -f "$filear" ]; then
-	    #cp $filear $volatile_recon_dir/$rnb/
-	    #fi
-	    ((i3++))
-	fi
-	if [[ -e $file2a || -e $file2b ]]; then
-	    #if [ -f "$filear" ]; then
-	    #cp $fileaf $volatile_flux_dir/$rnb/
-	    #fi
-	    ((i4++))
-	fi
-	if [[ -e $file3a || -e $file3b ]]; then
-	    #if [ -f "$filehe" ]; then
-	    #cp $filehe $volatile_hists/$rnb/helicity_${rnb}_$enb.log
-	    #fi
-	    #if [ -f "$fileah" ]; then
-	    #cp $fileah $volatile_hists/$rnb/
-	    #fi
-	    ((i5++))
-	fi
-	((i6++))
-    done
-    if (( i7 == 0 )); then
-	echo "$rnb $enb" >> good-list-$rp.txt
+for RUN_NUMBER in "${RUN_NUMBERS[@]}"
+do
+  echo "run number: ${RUN_NUMBER}"
+  EVIO_FILES=($(ls "${RAW_DATA_DIR}/Run${RUN_NUMBER}/hd_rawdata_??????_???.evio"))
+  i7=0
+  # mkdir -p "${VOLATILE_HISTS_DIR}/${RUN_NUMBER}""
+  for EVIO_FILE in "${EVIO_FILES[@]}"
+  do
+    read RUN_NUMBER < <(echo "${EVIO_FILE}" | grep -o '[0-9]\+' | tail -n 2)
+    #TODO RUN_NUMBER from file name should be the same as RUN_NUMBER from list; check this and print warning if not
+    read FILE_NUMBER < <(echo "${EVIO_FILE}" | grep -o '[0-9]\+' | tail -n 1)
+    #echo "run number: ${RUN_NUMBER} - file number: ${FILE_NUMBER}"
+    #echo "file: ${EVIO_FILE}"
+    #TODO improve logic and code
+    file1a="${RECON_DIR_CACHE}/REST/${RUN_NUMBER}/dana_rest_${RUN_NUMBER}_${FILE_NUMBER}.hddm"
+    file2a="${RECON_DIR_CACHE}/tree_PSFlux/${RUN_NUMBER}/tree_PSFlux_${RUN_NUMBER}_${FILE_NUMBER}.root"
+    file3a="${RECON_DIR_CACHE}/hists/${RUN_NUMBER}/hd_root_${RUN_NUMBER}_${FILE_NUMBER}.root"
+    file1b="${RECON_DIR_MSS}/REST/${RUN_NUMBER}/dana_rest_${RUN_NUMBER}_${FILE_NUMBER}.hddm"
+    file2b="${RECON_DIR_MSS}/tree_PSFlux/${RUN_NUMBER}/tree_PSFlux_${RUN_NUMBER}_${FILE_NUMBER}.root"
+    file3b="${RECON_DIR_MSS}/hists/${RUN_NUMBER}/hd_root_${RUN_NUMBER}_${FILE_NUMBER}.root"
+    #filehe="${WORK_DIR}/job_info_${RUN_NUMBER}_${FILE_NUMBER}/helicity.log"
+    filear="${VOLATILE_DIR}/REST/${RUN_NUMBER}/dana_rest_${RUN_NUMBER}_${FILE_NUMBER}.hddm"
+    fileaf="${VOLATILE_DIR}/tree_PSFlux/${RUN_NUMBER}/tree_PSFlux_${RUN_NUMBER}_${FILE_NUMBER}.root"
+    fileah="${VOLATILE_DIR}/hists/${RUN_NUMBER}/hd_root_${RUN_NUMBER}_${FILE_NUMBER}.root"
+    if { [[ -f "${file1a}" ]] || [[ -f "${file1b}" ]]; } && \
+       { [[ -f "${file2a}" ]] || [[ -f "${file2b}" ]]; } && \
+       { [[ -f "${file3a}" ]] || [[ -f "${file3b}" ]]; }
+    then
+      #echo "${RUN_NUMBER} ${FILE_NUMBER}" >> "good-list-${RUN_PERIOD}.txt"
+      ((i1++))
+    else
+      echo "${RUN_NUMBER} ${FILE_NUMBER}" >> "bad-list-${RUN_PERIOD}.txt"
+      #echo "Missing outputs ${RUN_NUMBER} ${FILE_NUMBER}"
+      ((i2++))
+      ((i7++))
     fi
+    #TODO also use -f below?
+    if [[ -e "${file1a}" || -e "${file1b}" ]]
+    then
+      # if [[ -f "${filear}" ]]
+      # then
+      #   cp "${filear}" "${VOLATILE_REST_DIR}/${RUN_NUMBER}"
+      # fi
+      ((i3++))
+    fi
+    if [[ -e "${file2a}" || -e "${file2b}" ]]
+    then
+      # if [[ -f "${fileaf}" ]]
+      # then
+      #   cp "${fileaf}" "${VOLATILE_FLUX_DIR}/${RUN_NUMBER}"
+      # fi
+      ((i4++))
+    fi
+    if [[ -e "${file3a}" || -e "${file3b}" ]]
+    then
+      # if [[ -f "${filehe}" ]]
+      # then
+      #   cp "${filehe}" "${VOLATILE_HISTS_DIR}/${RUN_NUMBER}/helicity_${RUN_NUMBER}_${FILE_NUMBER}.log"
+      # fi
+      # if [[ -f "${fileah}" ]]
+      # then
+      #   cp "${fileah}" "${VOLATILE_HISTS_DIR}/${RUN_NUMBER}"
+      # fi
+      ((i5++))
+    fi
+    ((i6++))
+  done
+  if (( i7 == 0 ))
+  then
+    echo "${RUN_NUMBER} ${FILE_NUMBER}" >> "good-list-${RUN_PERIOD}.txt"
+  fi
 done
-echo "Total number of correct files with REST, PSFlux, and hists: $i1"
-echo "Total number of incorrect files: $i2"
-echo "Total number of correct files with REST: $i3"
-echo "Total number of correct files with PSFlux: $i4"
-echo "Total number of correct files with hists: $i5"
-echo "Total number of files: $i6"
+echo "Total number of correct files with REST, PSFlux, and hists: ${i1}"
+echo "Total number of incorrect files: ${i2}"
+echo "Total number of correct files with REST: ${i3}"
+echo "Total number of correct files with PSFlux: ${i4}"
+echo "Total number of correct files with hists: ${i5}"
+echo "Total number of files: ${i6}"
