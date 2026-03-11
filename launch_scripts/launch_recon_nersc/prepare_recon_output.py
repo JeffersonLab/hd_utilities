fdd9a90e931280b1265be8bd53455b0fcf43ad65
bgrube <bgrube@jlab.org>
2026-03-11 12:15:56 -0400

    Improve code structure by moving code that processes run, task, and file directories into separate functions.

--- launch_scripts/launch_recon_nersc/prepare_recon_output.py
+++ launch_scripts/launch_recon_nersc/prepare_recon_output.py
@@ -48,6 +48,76 @@ RECON_SUBDIR_INFO: dict[str, tuple[str, str]] = {
 }
 
 
+def process_file_dir(
+  file_number: int,
+  task_dir:    str,
+  run_number:  int,
+  target_dir:  str,
+) -> dict[str, str]:
+  """Process the file directory defined by the arguments and return a map from original file paths to new file paths."""
+  file_dir = f"{task_dir}/FILE{file_number:03d}"
+  if not os.path.isdir(file_dir):
+    raise ValueError(f"ERROR: '{file_dir}' does not exist or is not a directory; aborting.")
+  print(f"    Processing file directory '{file_dir}")
+  #TODO also check hd_root_log_files
+  file_path_map: dict[str, str] = {}  # map from original file name to new file name
+  for _, (file_base_name, file_type) in RECON_SUBDIR_INFO.items():  # loop over hd_root output files
+    file_name = f"hd_rawdata_{run_number:06d}_{file_number:03d}.{file_base_name}.{file_type}" if file_type == "evio" else f"{file_base_name}.{file_type}"
+    file_path = f"{file_dir}/{file_name}"
+    if not os.path.isfile(file_path):
+      print(f"WARNING: expected file '{file_path}' is missing; ignoring")
+      continue
+    # fix file names of evio files and make file names of non-evio files unique
+    new_file_name = f"{file_base_name}_{run_number:06d}_{file_number:03d}.{file_type}"
+    file_path_map[file_path] = f"{target_dir}/{new_file_name}"
+    print(f"      '{file_path}' -> '{target_dir}/{new_file_name}'")
+    # os.symlink(file_path, f"{target_dir}/{new_file_name}")
+  return file_path_map
+
+
+def process_task_dir(
+  task_index:             int,
+  run_number:             int,
+  run_dir:                str,
+  nmb_files:              int,
+  nmb_processes_per_task: int,
+  target_dir:             str,
+) -> dict[str, str]:
+  """Process the task directory defined by the arguments and return a map from original file paths to new file paths."""
+  task_dir = f"{run_dir}/TASK{task_index:03d}"
+  if not os.path.isdir(task_dir):
+    raise ValueError(f"ERROR: '{task_dir}' does not exist or is not a directory; aborting.")
+  print(f"  Processing task directory '{task_dir}")
+  node_log_files: list[str] = sorted(glob.glob(f"{task_dir}/node.*"))  #TODO move into separate function and check that all files are there
+  file_number_start = task_index * nmb_processes_per_task
+  file_number_end   = min(file_number_start + nmb_processes_per_task, nmb_files)
+  file_path_map: dict[str, str] = {}  # map from original file name to new file name
+  for file_number in range(file_number_start, file_number_end):  # loop over EVIO file numbers
+    file_path_map |= process_file_dir(file_number, task_dir, run_number, target_dir)
+  return file_path_map
+
+
+def process_run_dir(
+  run_number:             int,
+  output_root:            str,
+  raw_data_root:          str,
+  nmb_processes_per_task: int,
+  target_dir:             str,
+) -> dict[str, str]:
+  """Process the run directory defined by the arguments and return a map from original file paths to new file paths."""
+  job_log_files: list[str] = sorted(glob.glob(f"{output_root}/job_{run_number:06d}*"))  #TODO move into separate function and check that all files are there
+  run_dir = f"{output_root}/RUN{run_number:06d}"
+  file_path_map: dict[str, str] = {}  # map from original file name to new file name
+  if not os.path.isdir(run_dir):
+    print(f"WARNING: '{run_dir}' does not exist or is not a directory; ignoring.")
+    return file_path_map
+  print(f"Processing run directory '{run_dir}")
+  nmb_files, nmb_tasks, _, _, = get_job_size(run_number, raw_data_root, nmb_processes_per_task)
+  for task_index in range(nmb_tasks):  # loop over tasks
+    file_path_map |= process_task_dir(task_index, run_number, run_dir, nmb_files, nmb_processes_per_task, target_dir)
+  return file_path_map
+
+
 def main(args: argparse.Namespace) -> None:
   launch_config: dict[str, str | None] = get_config_dict_from_env_file(args.launch_env_file)
 
@@ -62,38 +132,7 @@ def main(args: argparse.Namespace) -> None:
 
   file_path_map: dict[str, str] = {}  # map from original file name to new file name
   for run_number in run_numbers:  # loop over runs
-    job_log_files: list[str] = sorted(glob.glob(f"{swif_output_root}/job_{run_number:06d}*"))  #TODO move into separate function and check that all files are there
-    run_dir = f"{swif_output_root}/RUN{run_number:06d}"
-    if not os.path.isdir(run_dir):
-      print(f"WARNING: '{run_dir}' does not exist or is not a directory; ignoring.")
-      continue
-    print(f"Processing run directory '{run_dir}")
-    nmb_files, nmb_tasks, _, _, = get_job_size(run_number, swif_raw_data_root, nersc_nmb_processes_per_task)
-    for task_index in range(nmb_tasks):  # loop over tasks
-      task_dir = f"{run_dir}/TASK{task_index:03d}"
-      if not os.path.isdir(task_dir):
-        raise ValueError(f"ERROR: '{task_dir}' does not exist or is not a directory; aborting.")
-      print(f"  Processing task directory '{task_dir}")
-      node_log_files: list[str] = sorted(glob.glob(f"{task_dir}/node.*"))  #TODO move into separate function and check that all files are there
-      file_number_start = task_index * nersc_nmb_processes_per_task
-      file_number_end   = min(file_number_start + nersc_nmb_processes_per_task, nmb_files)
-      for file_number in range(file_number_start, file_number_end):  # loop over EVIO file numbers
-        file_dir = f"{task_dir}/FILE{file_number:03d}"
-        if not os.path.isdir(file_dir):
-          raise ValueError(f"ERROR: '{file_dir}' does not exist or is not a directory; aborting.")
-        print(f"    Processing file directory '{file_dir}")
-        #TODO also check hd_root_log_files
-        for _, (file_base_name, file_type) in RECON_SUBDIR_INFO.items():  # loop over hd_root output files
-          file_name = f"hd_rawdata_{run_number:06d}_{file_number:03d}.{file_base_name}.{file_type}" if file_type == "evio" else f"{file_base_name}.{file_type}"
-          file_path = f"{file_dir}/{file_name}"
-          if not os.path.isfile(file_path):
-            print(f"WARNING: expected file '{file_path}' is missing; ignoring")
-            continue
-          # fix file names of evio files and make file names of non-evio files unique
-          new_file_name = f"{file_base_name}_{run_number:06d}_{file_number:03d}.{file_type}"
-          file_path_map[file_path] = f"{target_dir}/{new_file_name}"
-          print(f"      '{file_path}' -> '{target_dir}/{new_file_name}'")
-          # os.symlink(file_path, f"{target_dir}/{new_file_name}")
+    file_path_map |= process_run_dir(run_number, swif_output_root, swif_raw_data_root, nersc_nmb_processes_per_task, target_dir)
 
 
 if __name__ == "__main__":
