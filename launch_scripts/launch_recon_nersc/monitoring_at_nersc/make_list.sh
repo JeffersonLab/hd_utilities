c3b928f60df36f48c2fa9de97fbd8f37c3a1b3b6
bgrube <bgrube@jlab.org>
2026-04-01 11:26:04 -0400

    Remove obsolete scripts.

--- launch_scripts/launch_recon_nersc/monitoring_at_nersc/make_list.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Makes lists of jobs in the queue at NERSC, both for all users and
-# for user `jlab` specifically.
-
-
-squeue >| list-queue.txt
-squeue --user=jlab >| list-jlab-queue.txt
