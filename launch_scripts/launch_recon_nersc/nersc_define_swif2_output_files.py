f7e1a0b04a78cb77e54da1b2f3382c2f1cb365f1
bgrube <bgrube@jlab.org>
2026-04-06 15:20:34 -0400

    Prevent accidentally appending files to `__swif_outfiles__`.

--- launch_scripts/launch_recon_nersc/nersc_define_swif2_output_files.py
+++ launch_scripts/launch_recon_nersc/nersc_define_swif2_output_files.py
@@ -8,6 +8,12 @@
   Use this to recover jobs that were killed due to Slurm timeout or
   other failures that prevented the job script `script_job.py` from
   finishing properly.
+
+  Example usage:
+    nersc_define_swif2_output_files.py \
+      --run_working_dir /pscratch/sd/j/jlab/swif/jobs/gxproj4/GlueX_recon_100553/70263838 \
+      --swif_output_root /lustre/expphy/volatile/halld/offsite_prod/RunPeriod-2022-05/recon/ver02-perl \
+    >> /pscratch/sd/j/jlab/swif/jobs/gxproj4/GlueX_recon_100553/70263838/job_100553_50929462.out
 """
 
 import argparse
@@ -50,9 +56,12 @@ def main(args: argparse.Namespace) -> None:
     sys.exit(1)
   run_number = int(run_dir[0][3:])
 
-  # define output files for swif2
-  os.environ["SWIF_JOB_STAGE_DIR"] = os.path.abspath(os.getcwd())  # needed by `./.swif/swif2` command
-  define_swif2_output_files(run_number, args.swif_output_root)
+  if os.path.exists(f"./__swif_outfiles__"):
+    raise RuntimeError(f"'__swif_outfiles__' already exists in '{args.run_working_dir}'; remove it before running this script")
+  else:
+    # define output files for swif2
+    os.environ["SWIF_JOB_STAGE_DIR"] = os.path.abspath(os.getcwd())  # needed by `./.swif/swif2` command
+    define_swif2_output_files(run_number, args.swif_output_root)
 
   print("-------------------------------------------------------------------------------")
   elapsed_time = int(time.time() - start_time)
