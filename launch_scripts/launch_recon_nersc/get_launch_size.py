049f70338b31c07f641eadffff8f3ee7daf23adc
bgrube <bgrube@jlab.org>
2026-04-16 20:05:56 -0400

    Improve EVIO file size plot.

--- launch_scripts/launch_recon_nersc/get_launch_size.py
+++ launch_scripts/launch_recon_nersc/get_launch_size.py
@@ -8,9 +8,11 @@ the number of NERSC nodes required to process them.
 from __future__ import annotations
 
 import argparse
+import functools
 import math
+import numpy as np
 import time
-from typing import Iterable
+from typing import Sequence
 
 import ROOT
 
@@ -25,26 +27,49 @@ from utilities import (
 )
 
 
+# always flush print() to reduce garbling of log files due to buffering
+print = functools.partial(print, flush = True)
+
+
 def plot_evio_file_size(
-  run_numbers:   Iterable[int],
+  run_numbers:   Sequence[int],
   raw_data_root: str,
+  swif_workflow: str,
 ) -> None:
   """Plots the distribution of EVIO file sizes for the given run numbers."""
   print(f"Plotting EVIO file size distribution for {len(run_numbers)} runs...")
   # get sizes of all EVIO files for the given runs
-  file_sizes_gb: list[float] = []  # file sizes in GB
+  file_sizes_GB: list[float] = []  # file sizes in GB
   for run_number in run_numbers:
     for evio_file_path in get_evio_file_paths_for_run(run_number, raw_data_root):
-      file_sizes_gb.append(float(get_file_size_from_mss_stub(evio_file_path)) / 1024**3)
+      file_sizes_GB.append(float(get_file_size_from_mss_stub(evio_file_path)) / 1024**3)
   # histogram file sizes
   canv = ROOT.TCanvas()
-  hist = ROOT.TH1F("evio_file_size", ";EVIO File Size (GB);Count", 100, 0, math.ceil(max(file_sizes_gb) * 1.05))
-  for size_gb in file_sizes_gb:
-    hist.Fill(size_gb)
+  hist = ROOT.TH1F(f"evio_file_size.{swif_workflow}", f"{swif_workflow};EVIO File Size (GB);Count", 200, 0, math.ceil(max(file_sizes_GB) * 1.05))
+  for size_GB in file_sizes_GB:
+    hist.Fill(size_GB)
   canv.SetLogy()
   hist.SetMinimum(0.1)  # improve log scale
+  hist.SetFillColor(ROOT.kBlue - 10)
   hist.Draw()
-  #TODO print total, average, and median file size on plot
+  #TODO print # of files, total, average, and median file size on plot, file size standard deviation, and maybe also min and max file size
+  nmb_files       = len(file_sizes_GB)
+  min_size_GB     = min(file_sizes_GB)
+  max_size_GB     = max(file_sizes_GB)
+  mean_size_GB    = hist.GetMean()
+  std_dev_size_GB = hist.GetRMS()
+  probabilities   = np.array([0.5])  # array of cumulative probabilities where quantiles should be evaluated
+  quantiles_GB    = np.array([0.])   # array to be filled with quantiles; must be same length as probabilities
+  hist.GetQuantiles(1, quantiles_GB, probabilities)
+  label = ROOT.TLatex()
+  label.SetNDC()
+  label.SetTextSize(0.04)
+  label.SetTextAlign(ROOT.kHAlignLeft + ROOT.kVAlignBottom)
+  label.DrawLatex(0.135, 0.86, f"{len(run_numbers)} runs with {nmb_files} files and total size of {sum(file_sizes_GB) / 1024:.1f} TB")
+  label.DrawLatex(0.135, 0.80, f"Min size = {min_size_GB:.1f} GB, max size = {max_size_GB:.1f} GB")
+  label.DrawLatex(0.135, 0.74, f"Mean size = {mean_size_GB:.1f} GB, median size = {quantiles_GB[0]:.1f} GB")
+  label.DrawLatex(0.135, 0.68, f"Size standard deviation = {std_dev_size_GB:.1f} GB")
+  canv.RedrawAxis()
   canv.SaveAs(f"./{hist.GetName()}.pdf")
 
 
@@ -55,6 +80,7 @@ def main(args: argparse.Namespace) -> None:
   run_period                   =     ensure_dict_value_exists(launch_config, "RUN_PERIOD")
   run_number_list_file         =     ensure_dict_value_exists(launch_config, "RUN_NUMBER_LIST_FILE") if args.override_run_list is None else args.override_run_list
   swif_raw_data_root           =     ensure_dict_value_exists(launch_config, "SWIF_RAW_DATA_ROOT")
+  swif_workflow                =     ensure_dict_value_exists(launch_config, "SWIF_WORKFLOW")
   nmb_processes_per_nersc_node = int(ensure_dict_value_exists(launch_config, "NERSC_NMB_PROCESSES_PER_TASK"))
   print(f"Allocating {nmb_processes_per_nersc_node} hd_root processes per NERSC node")
 
@@ -89,7 +115,7 @@ def main(args: argparse.Namespace) -> None:
           f"    out of which {total_nmb_nodes_unused:.1f} nodes are unused (= {total_nmb_nodes_unused / total_nmb_nodes:.1%} of total nodes)")
 
   print("-------------------------------------------------------------------------------")
-  plot_evio_file_size(run_numbers, swif_raw_data_root)
+  plot_evio_file_size(run_numbers, swif_raw_data_root, swif_workflow)
 
   print("-------------------------------------------------------------------------------")
   elapsed_time_sec = int(time.time() - start_time)
