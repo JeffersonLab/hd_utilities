5288534409174d2c4a3396155d0a23380353f50f
bgrube <bgrube@jlab.org>
2026-04-21 11:57:16 -0400

    Add script that uses `jmigrate` to copy data to tape.

---
+++ launch_scripts/launch_recon_nersc/copy_to_tape.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# copies the full content of the given source directory to tape using `jmigrate`
+#NOTE needs to be run periodically until all files have been copied to tape
+#NOTE running multiple instances of this script in parallel is not supported
+#NOTE the preferred way to copy data to tape is to submit a dedicated swif2 job using `submit_copy_dir_to_tape.py`
+
+
+LAUNCH_ENV_FILE="${1:-launch.env}"  # path to .env file defining the configuration variables of the reconstruction launch
+echo "Reading production parameters from .env file '${LAUNCH_ENV_FILE}'"
+source "${LAUNCH_ENV_FILE}"
+RECON_SRC_PATH="/lustre24/expphy/volatile/halld/offsite_prod/RunPeriod-2022-05/recon/ver02-perl.ready_for_tape.2/job_info"  # absolute path to the prepared reconstruction directory, the content of which will be copied to tape
+SRC_PATH_REPLACE=""/lustre24/expphy/volatile/halld/offsite_prod/RunPeriod-2022-05/recon/ver02-perl.ready_for_tape.2""  # part of path to replace by DEST_PATH_REPLACE
+DEST_PATH_REPLACE="/mss/halld/RunPeriod-2022-05/recon/ver02"  # path that replaces SRC_PATH_REPLACE
+export JMIRROR_LOG_DIR="${PRODUCTION_LAUNCH_DIR}/jmigrate_logs"
+# debug
+# RECON_SRC_PATH="/w/halld-scshelf2101/bgrube/halldRepos/hd_utilities/launch_scripts/launch_recon_nersc/test/test_work_dir_job"
+# SRC_PATH_REPLACE="${RECON_SRC_PATH}"
+# DEST_PATH_REPLACE="/mss/halld/home/bgrube/test/test_copy_dir_to_tape2"
+# export JMIRROR_LOG_DIR="./jmigrate_logs"
+
+# export JMIRROR_MIN_MODIFICATION_AGE_SECONDS=600  # default is 1800 seconds, 0 means copy all files regardless of modification time
+echo "Copying files from '${RECON_SRC_PATH}' to tape, replacing '${SRC_PATH_REPLACE}' with '${DEST_PATH_REPLACE}'"
+JMIGRATE_CMD=(jmigrate
+  # -verbose  # not very helpful
+  -delete written
+  "${RECON_SRC_PATH}"
+  "${SRC_PATH_REPLACE}"
+  "${DEST_PATH_REPLACE}"
+)
+echo "Running ${JMIGRATE_CMD[*]}"
+"${JMIGRATE_CMD[@]}"
+echo "See logs in '${JMIRROR_LOG_DIR}'"
