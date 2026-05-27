70e673bc15cbda2a8dd123911b8545281cb2f94c
bgrube <bgrube@jlab.org>
2026-05-27 11:23:37 -0400

    Require Python 3.9 for `nersc_define_swif2_output_files.py`. Simplify interface: use run number and optional swif attempt ID to find working directory of job. Add option to transfer all files regardless of `hd_root` return code. Improve log output.

--- launch_scripts/launch_recon_nersc/nersc_define_swif2_output_files.py
+++ launch_scripts/launch_recon_nersc/nersc_define_swif2_output_files.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-#NOTE this script needs to be compatible with Python 3.6
+#NOTE this script requires at least Python 3.9
+# run `module load python/3.9` at NERSC to get Python 3.9
 
 """
-Defines the output files that should be transferred back to JLab
-using `swif2 output` commands for the given run number.
+Define the output files that should be transferred back to JLab for
+the job corresponding to the given run number.
 
 Use this to recover jobs that were killed due to Slurm timeout or
 other failures that prevented the job script `script_job.py` from
 finishing properly.
 
 Example usage:
-  ./nersc_define_swif2_output_files.py \
-    --run_working_dir /pscratch/sd/j/jlab/swif/jobs/gxproj4/GlueX_recon_100570/70263844 \
-    --swif_output_root /lustre/expphy/volatile/halld/offsite_prod/RunPeriod-2022-05/recon/ver02-perl \
+Run
+  ./nersc_define_swif2_output_files.py 100570 \
   >> /pscratch/sd/j/jlab/swif/jobs/gxproj4/GlueX_recon_100570/70263844/job_100570_50937509.out
 and then bless the job
-  swif2 bless-jobs recon_2022-05_ver02-perl_NERSC-multi 55121879
+  swif2 bless-jobs recon_2022-05_ver02-perl_NERSC-multi GlueX_recon_100570
 """
+
+from __future__ import annotations
 
 import argparse
 import functools
@@ -37,34 +39,41 @@ print = functools.partial(print, flush = True)
 
 def main(args: argparse.Namespace) -> None:
   start_time = time.time()
+  print_command_line_arguments(args)
 
-  # go into run working directory
-  try:
-    os.chdir(args.run_working_dir)
-  except Exception as e:
-    print(f"Error: Failed to change into run working directory '{args.run_working_dir}': {e}")
+  # cd into run working directory
+  run_working_dir = f"{args.nersc_swif_jobs_root_dir}/GlueX_recon_{args.run_number:06d}"
+  os.chdir(run_working_dir)
+  # find attempt subdirectory to process and cd into it
+  swif_attempt_dirs = [entry for entry in sorted(glob.glob("*")) if os.path.isdir(entry)]
+  if len(swif_attempt_dirs) == 0:
+    print(f"Error: No directories with swif attempts found in '{run_working_dir}'")
     sys.exit(1)
-  # find RUNXXXXXX subdirectory and extract run number from it
-  run_dir = sorted(glob.glob(f"RUN??????"))
-  if len(run_dir) == 0:
-    print(f"Error: No run directory found in '{args.run_working_dir}'")
-    sys.exit(1)
-  elif len(run_dir) > 1:
-    print(f"Error: Multiple run directories found in '{args.run_working_dir}': {run_dir}")
-    sys.exit(1)
-  elif not run_dir[0][3:].isdigit():
-    print(f"Error: Invalid run directory name '{run_dir[0]}' in '{args.run_working_dir}'")
-    sys.exit(1)
-  run_number = int(run_dir[0][3:])
+  if args.swif_attempt_id is not None:
+    # find matching swif attempt dir
+    found_matching_swif_attempt_dir = False
+    for swif_attempt_dir in swif_attempt_dirs:
+      if os.path.basename(swif_attempt_dir) == str(args.swif_attempt_id):
+        found_matching_swif_attempt_dir = True
+        print(f"Found directory '{swif_attempt_dir}' for swif attempt ID {args.swif_attempt_id}")
+        os.chdir(swif_attempt_dir)
+        break
+    if not found_matching_swif_attempt_dir:
+      print(f"Error: No matching swif attempt directory for ID {args.swif_attempt_id} found in '{run_working_dir}'")
+      sys.exit(1)
+  else:
+    if len(swif_attempt_dirs) > 1:
+      print(f"Warning: Multiple directories with swif attempts found in '{run_working_dir}': {swif_attempt_dirs}; using the last one")
+    os.chdir(swif_attempt_dirs[-1])
+  print(f"Processing swif attempt directory '{os.getcwd()}'")
 
   # protect against accidentally appending files to existing `__swif_outfiles__` file
   if os.path.exists(f"./__swif_outfiles__"):
-    raise RuntimeError(f"'__swif_outfiles__' already exists in '{args.run_working_dir}'; remove it before running this script")
+    raise RuntimeError(f"'__swif_outfiles__' already exists in '{run_working_dir}'; remove it before running this script")
   else:
-    print_command_line_arguments(args)
     # define output files for swif2
     os.environ["SWIF_JOB_STAGE_DIR"] = os.path.abspath(os.getcwd())  # needed by `./.swif/swif2` command
-    define_swif2_output_files(run_number, args.swif_output_root)
+    define_swif2_output_files(args.run_number, args.swif_output_root, args.transfer_all_files)
 
   print("-------------------------------------------------------------------------------")
   elapsed_time_sec = int(time.time() - start_time)
@@ -73,8 +82,11 @@ def main(args: argparse.Namespace) -> None:
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
-    description = "Define the output files that should be transferred back to JLab using `swif2 output` commands for the given run number.",
+    description = "Define the output files that should be transferred back to JLab for the job corresponding to the given run number.",
   )
-  parser.add_argument("--run_working_dir",  required = True, help = "Working directory of the run")
-  parser.add_argument("--swif_output_root", required = True, help = "Root of JLab directory tree, where output files will be copied to")  #TODO make optional and take default from .env file or from info job .out file
+  parser.add_argument("run_number", type = int, help = "Run number of the job to process")
+  parser.add_argument("--swif_attempt_id", type = int, help = "SWIF attempt ID of the job to process; if not given, the last attempt will be used")
+  parser.add_argument("--nersc_swif_jobs_root_dir", default = "/pscratch/sd/j/jlab/swif/jobs/gxproj4", help = "NERSC root directory for SWIF jobs; default: '%(default)s'")
+  parser.add_argument("--swif_output_root", default = "/lustre/expphy/volatile/halld/offsite_prod/RunPeriod-2021-11/recon/ver05-perl", help = "Root of JLab directory tree, where output files will be copied to; default: '%(default)s'")  #TODO read this value from the jobs .env file
+  parser.add_argument("--transfer_all_files", action = "store_true", help = "If set, do not filter output of failed hd_root processes; default: False")
   main(parser.parse_args())
