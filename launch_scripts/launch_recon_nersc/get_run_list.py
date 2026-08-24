d7d1e2245ddc6dae6003c12eb546087d07d87c7a
bgrube <bgrube@jlab.org>
2026-08-24 17:49:15 -0400

    Improve naming.

--- launch_scripts/launch_recon_nersc/get_run_list.py
+++ launch_scripts/launch_recon_nersc/get_run_list.py
@@ -34,43 +34,43 @@ class EvioFilesErrorKind(Enum):
   MISSING_EVIO_FILE   = auto()
 
 @dataclass
-class EvioFilesError:
+class EvioFileError:
   """Class holding error information when getting list of EVIO files."""
-  kind:   EvioFilesErrorKind
-  detail: str | None = None
+  errorType: EvioFileErrorType
+  detail:    str | None = None
 
-def get_evio_files_for_run(
+def get_evio_file_paths_for_run(
   run_info:      rcdb.model.Run,  # RCDB run info object to read information from
   raw_data_root: str,             # root dir for EVIO files
-) -> tuple[list[str], list[EvioFilesError]]:
+) -> tuple[list[str], list[EvioFileError]]:
   """Gets list of EVIO files for the given RCDB run-info object."""
   run_number = int(run_info.number)
   raw_data_dir_run = f"{raw_data_root}/Run{run_number:06d}"
   evio_files_count = run_info.get_condition_value("evio_files_count")  # get number of evio files for this run; file numbers are in range 0 to evio_files_count - 1
-  rcdb_errors: list[EvioFilesError] = []
+  rcdb_errors: list[EvioFileError] = []
   if evio_files_count is None:
     print(f"WARNING: RCDB does not contain a value for `evio_files_count` for run {run_number}")
-    rcdb_errors.append(EvioFilesError(kind = EvioFilesErrorKind.NO_RCDB_FILES_COUNT))
+    rcdb_errors.append(EvioFileError(errorType = EvioFileErrorType.NO_RCDB_FILES_COUNT))
     evio_pattern = f"{raw_data_dir_run}/hd_rawdata_{run_number:06d}_???.evio"
     print(f"Counting files matching '{evio_pattern}' instead")
     evio_files_count = len(glob.glob(evio_pattern))
   if evio_files_count is None or evio_files_count == 0:
     print(f"WARNING: could not find any EVIO files for run {run_number}")
-    rcdb_errors.append(EvioFilesError(kind = EvioFilesErrorKind.NO_FILES_FOUND))
+    rcdb_errors.append(EvioFileError(errorType = EvioFileErrorType.NO_FILES_FOUND))
     return [], rcdb_errors
   print(f"Ensuring all {evio_files_count:3d} EVIO files for run {run_number} exist")
-  evio_files: list[str] = []
+  evio_file_paths: list[str] = []
   for evio_file_index in range(evio_files_count):
     evio_file_path = f"{raw_data_dir_run}/hd_rawdata_{run_number:06d}_{evio_file_index:03d}.evio"
     if not os.path.isfile(evio_file_path):
       print(f"WARNING: expected EVIO file '{evio_file_path}' does not exist")
-      rcdb_errors.append(EvioFilesError(kind = EvioFilesErrorKind.MISSING_EVIO_FILE, detail = evio_file_path))
+      rcdb_errors.append(EvioFileError(errorType = EvioFileErrorType.MISSING_EVIO_FILE, detail = evio_file_path))
     else:
-      evio_files.append(evio_file_path)
-  return evio_files, rcdb_errors
+      evio_file_paths.append(evio_file_path)
+  return evio_file_paths, rcdb_errors
 
 
-def get_evio_files(
+def get_evio_file_paths(
   db:            rcdb.RCDBProvider,  # RCDB object to read run information from
   run_period:    str,                # e.g. '2022-05'
   run_list:      list[int],          # list of runs to process
@@ -78,37 +78,37 @@ def get_evio_files(
 ) -> dict[int, list[str]]:
   """Gets the list of EVIO files for each run in the given list of runs, and also tallies any errors encountered in the process."""
   print(f"Searching for raw-data .evio files in '{raw_data_root}'")
-  evio_files_per_run: dict[int, list[str]] = {}
-  error_counts: dict[EvioFilesErrorKind, int] = {kind : 0 for kind in EvioFilesErrorKind}
+  evio_file_paths_per_run: dict[int, list[str]] = {}
+  error_counts: dict[EvioFileErrorType, int] = {errorType : 0 for errorType in EvioFileErrorType}
   for run_number in run_list:
     # get run information from RCDB
     run_info = db.get_run(run_number)
     # get list of evio files for this run, and any errors encountered in the process
-    evio_files:  list[str]
-    rcdb_errors: list[EvioFilesError]
-    evio_files, rcdb_errors = get_evio_files_for_run(run_info, raw_data_root)
-    evio_files_per_run[run_number] = evio_files
+    evio_file_paths: list[str]
+    rcdb_errors:     list[EvioFileError]
+    evio_file_paths, rcdb_errors = get_evio_file_paths_for_run(run_info, raw_data_root)
+    evio_file_paths_per_run[run_number] = evio_file_paths
     # tally any errors returned for this run
     for err in rcdb_errors:
-      error_counts[err.kind] += 1
-  total_nmb_evio_files = sum(len(files) for files in evio_files_per_run.values())
+      error_counts[err.errorType] += 1
+  total_nmb_evio_files = sum(len(file_paths) for file_paths in evio_file_paths_per_run.values())
   print("============================================================================================")
   print("Summary")
   print("--------------------------------------------------------------------------------------------")
   print(f"            Run period: {run_period}")
-  print(f"        Number of runs: {len(evio_files_per_run)}")
+  print(f"        Number of runs: {len(evio_file_paths_per_run)}")
   print(f"  Number of EVIO files: {total_nmb_evio_files}")
   print("--------------------------------------------------------------------------------------------")
   print("Error summary")
   print("--------------------------------------------------------------------------------------------")
-  print(f"  {error_counts[EvioFilesErrorKind.NO_RCDB_FILES_COUNT]:5d} runs with no `evio_files_count value` in RCDB")
+  print(f"  {error_counts[EvioFileErrorType.NO_RCDB_FILES_COUNT]:5d} runs with no `evio_files_count` value in RCDB")
   print(f"          for these runs the number of EVIO files was estimated by counting files in `/mss`")
-  print(f"  {error_counts[EvioFilesErrorKind.NO_FILES_FOUND     ]:5d} runs with no EVIO files found")
+  print(f"  {error_counts[EvioFileErrorType.NO_FILES_FOUND     ]:5d} runs with no EVIO files found")
   print(f"          for these runs no jobs will be submitted")
-  print(f"  {error_counts[EvioFilesErrorKind.MISSING_EVIO_FILE  ]:5d} EVIO files not found")
+  print(f"  {error_counts[EvioFileErrorType.MISSING_EVIO_FILE  ]:5d} EVIO files not found")
   print(f"          for these files no jobs will be submitted")
   print("============================================================================================")
-  return evio_files_per_run
+  return evio_file_paths_per_run
 
 
 def main(args: argparse.Namespace) -> None:
@@ -134,12 +134,12 @@ def main(args: argparse.Namespace) -> None:
     run_list = read_run_numbers_from_file(args.override_run_list)
   print(f"Found {len(run_list)} runs")
 
-  evio_files_per_run: dict[int, list[str]] = get_evio_files(db, run_period, run_list, raw_data_root)
+  evio_file_paths_per_run: dict[int, list[str]] = get_evio_file_paths(db, run_period, run_list, raw_data_root)
   print(f"Writing list of run numbers to './{run_number_list_file}'")
   try:
     with open(f"./{run_number_list_file}", mode = "x") as file:
-      for run_number in sorted(evio_files_per_run.keys()):
-        if len(evio_files_per_run[run_number]) > 0:
+      for run_number in sorted(evio_file_paths_per_run.keys()):
+        if len(evio_file_paths_per_run[run_number]) > 0:
           file.write(f"{run_number:06d}\n")
   except FileExistsError:
     print(f"WARNING: file './{run_number_list_file}' already exists; skipping write")
